2e7a2bf3a3356742133f73e0557ddf6dfcc9f9ba
franco.stramana <franco.stramana@gmail.com>
2021-06-20 19:16:12 -0300

    fix unit test

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -3,6 +3,8 @@ import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
 
+jest.mock('../renderer/dialog-controller.ts');
+
 describe('App', () => {
   it('should render', () => {
     expect(render(<App />)).toBeTruthy();
