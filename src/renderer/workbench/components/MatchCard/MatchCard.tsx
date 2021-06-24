813132abbe0cbe909b9f56d1556eeb3e413d775c
Cancu <juancruz.cancu@gmail.com>
2021-06-24 09:39:29 -0300

    Added hover to match card --  not finished

--- src/renderer/workbench/components/MatchCard/MatchCard.tsx
+++ src/renderer/workbench/components/MatchCard/MatchCard.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import CheckIcon from '@material-ui/icons/Check';
+import BanIcon from '@material-ui/icons/NotInterested';
 import Label from '../Label/Label';
 
 interface LabelType {
@@ -8,9 +10,15 @@ interface LabelType {
 const MatchCard = ({ labelOfCard, status }) => {
   //
   return (
-    <div className={`match-card status-${status.toLowerCase()}`  }>
-      <div>
-        <Label label={labelOfCard} textColor="black" />
+    <div className={`match-card status-${status.toLowerCase()}`}>
+      <div className="match-card-content">
+        <div className="label-div">
+          <Label label={labelOfCard} textColor="black" />
+        </div>
+        <div className="match-card-buttons">
+          <CheckIcon className="icon check" />
+          <BanIcon className="icon ban" />
+        </div>
       </div>
     </div>
   );
