61225a01e55a6cf7d72b910e51cce32d0ffcc71b
Cancu <juancruz.cancu@gmail.com>
2021-07-08 13:54:53 -0300

    popover added

--- src/renderer/workbench/components/ComponentInfo/ComponentInfo.tsx
+++ src/renderer/workbench/components/ComponentInfo/ComponentInfo.tsx
@@ -1,16 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Popover from '@material-ui/core/Popover';
 import { Component } from '../../WorkbenchProvider';
 import Label from '../Label/Label';
 import Title from '../Title/Title';
+import componentDefault from '';
 
-export const ComponentInfo = ({component}: {component: Component}) => {
+export const ComponentInfo = ({ component }: { component: Component }) => {
+  const [over, setOver] = useState<boolean>(false);
+
+  const useStyles = makeStyles((theme) => ({
+    popover: {
+      pointerEvents: 'none',
+    },
+    paper: {
+      padding: theme.spacing(1),
+    },
+  }));
+  const classes = useStyles();
+
+  useEffect(() => {
+    console.log(over);
+  }, [over]);
   return (
     <div className="component-info">
-      <div>
+      <div
+        onMouseEnter={() => setOver(true)}
+        onMouseLeave={() => setOver(false)}
+        className="container-component-info"
+      >
         <Label label={component?.version} textColor="gray" />
         <Title title={component?.name} />
+
+        {over ? (
+          <div className="component-details-card">
+            <div className="tiny-container-detail">
+              <p className="title-detail">License</p>
+              <p className="desc-detail">{component?.name}</p>
+            </div>
+            <div className="tiny-container-detail">
+              <p className="title-detail">PURL</p>
+              <p className="desc-detail">{component?.purl}</p>
+            </div>
+            <div className="tiny-container-detail">
+              <p className="title-detail">URL</p>
+              <a href={component?.url} className="desc-detail url">
+                {component?.url}
+              </a>
+            </div>
+          </div>
+        ) : null}
       </div>
-      {/*<div>
+
+      {/* <div>
         <Label label="VENDOR" textColor="gray" />
         <Title title={component?.vendor} />
       </div>
@@ -27,9 +69,9 @@ export const ComponentInfo = ({component}: {component: Component}) => {
               : '-'
           }
         />
-      </div>*/}
+      </div> */}
     </div>
   );
-}
+};
 
 export default ComponentInfo;
