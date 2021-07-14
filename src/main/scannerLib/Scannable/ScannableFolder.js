ca7ae8bb916f62f0b2c4cfebef3f1342a3f7a48e
Agustin <isasmendi.agus@gmail.com>
2021-07-14 17:53:07 -0300

    Added filters to scanFolder

--- src/main/scannerLib/Scannable/ScannableFolder.js
+++ src/main/scannerLib/Scannable/ScannableFolder.js
@@ -4,8 +4,132 @@ import fs from 'fs';
 import { AbstractScannable } from './AbstractScannable.js';
 import { ScannableItem } from './ScannableItem.js';
 
+const FILTERED_EXT = [
+  '.1',
+  '.2',
+  '.3',
+  '.4',
+  '.5',
+  '.6',
+  '.7',
+  '.8',
+  '.9',
+  '.ac',
+  '.adoc',
+  '.am',
+  '.asciidoc',
+  '.bmp',
+  '.build',
+  '.cfg',
+  '.chm',
+  '.class',
+  '.cmake',
+  '.cnf',
+  '.config',
+  '.contributors',
+  '.copying',
+  '.crt',
+  '.csproj',
+  '.css',
+  '.csv',
+  '.cvsignore',
+  '.dat',
+  '.data',
+  '.doc',
+  '.ds_store',
+  '.dtd',
+  '.dts',
+  '.dump',
+  '.eot',
+  '.eps',
+  '.geojson',
+  '.gdoc',
+  '.gif',
+  '.gitignore',
+  '.glif',
+  '.gmo',
+  '.gradle',
+  '.guess',
+  '.hex',
+  '.htm',
+  '.html',
+  '.ico',
+  '.in',
+  '.inc',
+  '.info',
+  '.ini',
+  '.ipynb',
+  '.jpeg',
+  '.jpg',
+  '.json',
+  '.jsonld',
+  '.log',
+  '.m4',
+  '.map',
+  '.markdown',
+  '.md',
+  '.md5',
+  '.meta',
+  '.mk',
+  '.mxml',
+  '.o',
+  '.otf',
+  '.out',
+  '.pbtxt',
+  '.pdf',
+  '.pem',
+  '.phtml',
+  '.plist',
+  '.png',
+  '.po',
+  '.ppt',
+  '.prefs',
+  '.properties',
+  '.pyc',
+  '.qdoc',
+  '.result',
+  '.rgb',
+  '.rst',
+  '.scss',
+  '.sha',
+  '.sha1',
+  '.sha2',
+  '.sha256',
+  '.sln',
+  '.spec',
+  '.sql',
+  '.sub',
+  '.svg',
+  '.svn-base',
+  '.tab',
+  '.template',
+  '.test',
+  '.tex',
+  '.tiff',
+  '.toml',
+  '.ttf',
+  '.txt',
+  '.utf-8',
+  '.vim',
+  '.wav',
+  '.whl',
+  '.woff',
+  '.xht',
+  '.xhtml',
+  '.xls',
+  '.xml',
+  '.xpm',
+  '.xsd',
+  '.xul',
+  '.yaml',
+  '.yml',
+];
+
+const FILTERED_DIRS = ['.git', '.svn', '.eggs', 'pycache', 'node_modules'];
+
 export class ScannableFolder extends AbstractScannable {
   // Private properties
+
   #folderPath;
 
   #generator;
@@ -18,12 +142,13 @@ export class ScannableFolder extends AbstractScannable {
   }
 
   async *#walk(dir) {
+    // eslint-disable-next-line no-restricted-syntax
     for await (const d of await fs.promises.opendir(dir)) {
       const entry = path.join(dir, d.name);
-      if (d.isDirectory() && !d.isSymbolicLink()) yield* this.#walk(entry);
-      else if (d.isFile() && !d.isSymbolicLink()) {
+      if (d.isDirectory() && !d.isSymbolicLink() && !this.#is_filtered_dir(entry)) yield* this.#walk(entry);
+      else if (d.isFile() && !d.isSymbolicLink() && !FILTERED_EXT.includes(path.extname(entry))) {
         const fileContent = await fs.promises.readFile(entry);
-        yield new ScannableItem(entry.replace(this.#folderPath,''), fileContent);
+        yield new ScannableItem(entry.replace(this.#folderPath, ''), fileContent);
       }
     }
   }
@@ -40,5 +165,20 @@ export class ScannableFolder extends AbstractScannable {
 
   prepare() {
     this.#generator = this.#walk(this.#folderPath);
+
+    // Prepare the FILTERED_DIRS with the specific OS path separator
+    const pathSeparator = path.sep;
+    for (let i = 0; i < FILTERED_DIRS.length; i += 1) {
+      FILTERED_DIRS[i] = pathSeparator + FILTERED_DIRS[i];
+    }
+  }
+
+  #is_filtered_dir(dir) {
+    for (let i = 0; i < FILTERED_DIRS.length; i += 1) {
+      if (dir.includes(FILTERED_DIRS[i])) {
+        return true;
+      }
+    }
+    return false;
   }
 }
