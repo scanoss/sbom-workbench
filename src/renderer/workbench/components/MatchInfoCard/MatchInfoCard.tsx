238b077355d5452b8097cf850f0c571efee4ea04
franco.stramana <franco.stramana@gmail.com>
2021-07-23 12:08:08 -0300

    fix bug hover

--- src/renderer/workbench/components/MatchInfoCard/MatchInfoCard.tsx
+++ src/renderer/workbench/components/MatchInfoCard/MatchInfoCard.tsx
@@ -35,87 +35,88 @@ const MatchInfoCard = ({ match, onSelect, status, selected, onAction }: MatchInf
 
   return (
     <>
-      <div
-        onClick={onSelect}
-        style={selected ? { border: '#60A5FA 2px solid' } : {}}
-        className="match-info-card"
+      <article className="match-info-card-container"
         onMouseEnter={() => setOver(true)}
-        onMouseLeave={() => setOver(false)}
-      >
-        <div className={`match-info-card-content status-${status}`}>
-          <div className="label-info-div">
-            <img alt="component logo" className="logo-match-info-card" src={componentDefault} />
-            <span className="component-span">{match.component}</span>
-            <span className="version-span">{match.version}</span>
-            <div className="usage-div">
-              {status === 'pending' || status === 'ignored' ? (
-                <span className="usage-label">DETECTED</span>
-              ) : (
-                <span className="usage-label">USAGE</span>
+        onMouseLeave={() => setOver(false)}>
+        <div
+          onClick={onSelect}
+          style={selected ? { border: '#60A5FA 2px solid' } : {}}
+          className="match-info-card"
+        >
+          <div className={`match-info-card-content status-${status}`}>
+            <div className="label-info-div">
+              <img alt="component logo" className="logo-match-info-card" src={componentDefault} />
+              <span className="component-span">{match.component}</span>
+              <span className="version-span">{match.version}</span>
+              <div className="usage-div">
+                {status === 'pending' || status === 'ignored' ? (
+                  <span className="usage-label">DETECTED</span>
+                ) : (
+                  <span className="usage-label">USAGE</span>
+                )}
+                <span className="usage-value">{match.usage}</span>
+              </div>
+            </div>
+            <div className="match-info-card-buttons">
+              {status === 'pending' && (
+                <>
+                  <Tooltip title="Identify">
+                    <IconButton onClick={() => onAction(MATCH_INFO_CARD_ACTIONS.ACTION_IDENTIFY)}>
+                      <CheckIcon className="icon check" />
+                    </IconButton>
+                  </Tooltip>
+                  <Tooltip title="Ignore">
+                    <IconButton onClick={() => onAction(MATCH_INFO_CARD_ACTIONS.ACTION_IGNORE)}>
+                      <BanIcon className="icon ban" />
+                    </IconButton>
+                  </Tooltip>
+                </>
               )}
-              <span className="usage-value">{match.usage}</span>
+              {status === 'ignored' && (
+                <>
+                  <Tooltip title="Restore">
+                    <IconButton onClick={() => onAction(MATCH_INFO_CARD_ACTIONS.ACTION_RESTORE)}>
+                      <RestoreOutlined className="icon" />
+                    </IconButton>
+                  </Tooltip>
+                </>
+              )}
+              {status === 'identified' && (
+                <>
+                  {/* <Tooltip title="Ignore">
+                  <IconButton onClick={() => onAction(MATCH_INFO_CARD_ACTIONS.ACTION_IGNORE)}>
+                      <BanIcon />
+                    </IconButton>
+                </Tooltip> */}
+                  <Tooltip title="Details">
+                    <IconButton onClick={() => onAction(MATCH_INFO_CARD_ACTIONS.ACTION_DETAIL)}>
+                      <DescriptionOutlined />
+                    </IconButton>
+                  </Tooltip>
+                </>
+              )}
             </div>
           </div>
-          <div className="match-info-card-buttons">
-            {status === 'pending' && (
-              <>
-                <Tooltip title="Identify">
-                  <IconButton onClick={() => onAction(MATCH_INFO_CARD_ACTIONS.ACTION_IDENTIFY)}>
-                    <CheckIcon className="icon check" />
-                  </IconButton>
-                </Tooltip>
-                <Tooltip title="Ignore">
-                  <IconButton onClick={() => onAction(MATCH_INFO_CARD_ACTIONS.ACTION_IGNORE)}>
-                    <BanIcon className="icon ban" />
-                  </IconButton>
-                </Tooltip>
-              </>
-            )}
-            {status === 'ignored' && (
-              <>
-                <Tooltip title="Restore">
-                  <IconButton onClick={() => onAction(MATCH_INFO_CARD_ACTIONS.ACTION_RESTORE)}>
-                    <RestoreOutlined className="icon" />
-                  </IconButton>
-                </Tooltip>
-              </>
-            )}
-            {status === 'identified' && (
-              <>
-                {/* <Tooltip title="Ignore">
-                <IconButton onClick={() => onAction(MATCH_INFO_CARD_ACTIONS.ACTION_IGNORE)}>
-                    <BanIcon />
-                  </IconButton>
-              </Tooltip> */}
-                <Tooltip title="Details">
-                  <IconButton onClick={() => onAction(MATCH_INFO_CARD_ACTIONS.ACTION_DETAIL)}>
-                    <DescriptionOutlined />
-                  </IconButton>
-                </Tooltip>
-              </>
-            )}
-          </div>
         </div>
-      </div>
-      {over ? (
-        <div className="component-details-card2">
-          <div className="tiny-container-detail2">
-            <p className="title-detail2">License</p>
-            <p className="desc-detail2">{match?.license?.name}</p>
+        {over ? (
+          <div className="component-details-card2">
+            <div className="tiny-container-detail2">
+              <p className="title-detail2">License</p>
+              <p className="desc-detail2">{match?.license?.name}</p>
+            </div>
+            <div className="tiny-container-detail2">
+              <p className="title-detail2">PURL</p>
+              <p className="desc-detail2">{match?.purl}</p>
+            </div>
+            <div className="tiny-container-detail2">
+              <p className="title-detail2">URL</p>
+              <a href={match?.url} target="_blank" className="desc-detail2 url2" rel="noreferrer">
+              {match?.url}
+              </a>
+            </div>
           </div>
-          <div className="tiny-container-detail2">
-            <p className="title-detail2">PURL</p>
-            <p className="desc-detail2">{match?.purl}</p>
-          </div>
-          <div className="tiny-container-detail2">
-            <p className="title-detail2">URL</p>
-            <a href={match?.url} target="_blank" className="desc-detail2 url2" rel="noreferrer">
-            {match?.url}
-            </a>
-          </div>
-        </div>
-      ) : null}
-
+        ) : null}
+      </article>
     </>
   );
 };
