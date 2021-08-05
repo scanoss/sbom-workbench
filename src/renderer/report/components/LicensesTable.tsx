8430c539af2afeea4d25ca606794ba093b22b62a
Cancu <juancruz.cancu@gmail.com>
2021-08-05 15:45:58 -0300

    colors added to  licenses

--- src/renderer/report/components/LicensesTable.tsx
+++ src/renderer/report/components/LicensesTable.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
 const LicensesTable = ({ data, selectLicense }) => {
+  const colors = ['#E8B34B', '#E22C2C', '#5754D0', '#9F69C0', '#FE7F10'];
+
+  const getColor = () => {};
+
   return (
     <div className="license-list">
       {data.map((item, index) => {
         return (
           <div onClick={() => selectLicense(item.label)} key={index} className="license-list-item">
-            <div className="license-list-item-color" />
+            <div style={{ backgroundColor: colors[index] }} className="license-list-item-color" />
             <div className="license-list-item-name">{item.label}</div>
           </div>
         );
