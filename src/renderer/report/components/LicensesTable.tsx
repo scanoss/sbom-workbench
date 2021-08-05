1485429b5eeeab3ca95fa815ce9446f31fe80d09
Cancu <juancruz.cancu@gmail.com>
2021-08-05 17:20:58 -0300

    getColor function moved to utils.js -- final commit

--- src/renderer/report/components/LicensesTable.tsx
+++ src/renderer/report/components/LicensesTable.tsx
@@ -1,38 +1,7 @@
 import React from 'react';
+import { getColor } from '../../../utils/utils';
 
 const LicensesTable = ({ data, selectLicense }) => {
-
-  const getColor = (index) => {
-    const colors = [
-      '#E8B34B',
-      '#E22C2C',
-      '#5754D0',
-      '#9F69C0',
-      '#FE7F10',
-      '#E56399',
-      '#E637BF',
-      '#474647',
-      '#153243',
-      '#2DE1C2',
-      '#F05365',
-      '#A2D729',
-      '#3C91E6',
-      '#FA824C',
-      '#C94277',
-      '#E56B6F',
-      '#F71735',
-      '#011627',
-      '#724E91',
-      '#7D451B',
-      '#9BE564',
-    ];
-    if (index < colors.length) {
-      return colors[index];
-    }
-    // repeat the array and return the same colors
-    return getColor(index % colors.length);
-  };
-
   return (
     <div className="license-list">
       {data.map((item, index) => {
