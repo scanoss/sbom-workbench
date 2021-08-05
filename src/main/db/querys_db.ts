f78d45dc0d8b653093ded9dd18cb771bac13fbd3
Agustin <agustin.groh@scanoss.com>
2021-08-05 16:21:39 -0300

    Add summary to GetComponentgroup and fix getAll components

--- src/main/db/querys_db.ts
+++ src/main/db/querys_db.ts
@@ -155,6 +155,8 @@ export class Querys {
 
   SQL_GET_SUMMARY_BY_PURL_VERSION = 'SELECT identified,pending,ignored FROM summary WHERE purl=? AND version=?;';
 
+  SQL_GET_SUMMARY_BY_PURL = 'SELECT identified,pending,ignored FROM summary WHERE purl=?;';
+
   SQL_GET_UNIQUE_COMPONENT = 'SELECT DISTINCT purl,version,license,component,url FROM results;';
 
   SQL_DELETE_INVENTORY_BY_ID = 'DELETE FROM inventories WHERE id=?;';
