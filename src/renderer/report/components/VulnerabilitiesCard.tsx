741674aeff540c47e8dd96e1702bb20686c0cfac
Cancu <juancruz.cancu@gmail.com>
2021-08-11 12:18:48 -0300

    Mayusc added to Vulnerabilities card

--- src/renderer/report/components/VulnerabilitiesCard.tsx
+++ src/renderer/report/components/VulnerabilitiesCard.tsx
@@ -5,19 +5,19 @@ const VulnerabilitiesCard = ({ data }) => {
     <div id="VulnerabilitiesContainer">
       <div className="vulnerabilitie-container">
         <span className="vulnerabilitie-number critical-number">{data?.critical}</span>
-        <span className="vulnerabilitie-label critical-label">Critical</span>
+        <span className="vulnerabilitie-label critical-label">CRITICAL</span>
       </div>
       <div className="vulnerabilitie-container high">
         <span className="vulnerabilitie-number">{data?.high}</span>
-        <span className="vulnerabilitie-label high">High</span>
+        <span className="vulnerabilitie-label high">HIGH</span>
       </div>
       <div className="vulnerabilitie-container medium">
         <span className="vulnerabilitie-number">{data?.medium}</span>
-        <span className="vulnerabilitie-label medium">Medium</span>
+        <span className="vulnerabilitie-label medium">MEDIUM</span>
       </div>
       <div className="vulnerabilitie-container moderate">
         <span className="vulnerabilitie-number">{data?.moderate}</span>
-        <span className="vulnerabilitie-label moderate">Moderate</span>
+        <span className="vulnerabilitie-label moderate">MODERATE</span>
       </div>
     </div>
   );
