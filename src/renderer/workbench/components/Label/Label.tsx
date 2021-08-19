22b9e68c18f047a300e65d963cec7369b912066a
franco.stramana <franco.stramana@gmail.com>
2021-08-19 12:05:11 -0300

    add copy file path to clipboard

--- src/renderer/workbench/components/Label/Label.tsx
+++ src/renderer/workbench/components/Label/Label.tsx
@@ -1,16 +1,28 @@
+import { Tooltip } from '@material-ui/core';
 import React from 'react';
 
 interface LabelType {
   label: string | null;
   textColor: string | null;
+  tooltip?: boolean;
 }
 
-const Label = ({ label, textColor }: LabelType) => {
+const Label = ({ label, textColor, tooltip }: LabelType) => {
   return (
     <>
-      <span className={`label-boxie-${textColor}`}>{label}</span>
+      {tooltip ? (
+        <Tooltip title={label || ''}>
+          <span className={`label-boxie-${textColor}`}>{label}</span>
+        </Tooltip>
+      ) : (
+        <span className={`label-boxie-${textColor}`}>{label}</span>
+      )}
     </>
   );
+};
+
+Label.defaultProps = {
+  tooltip: false,
 };
 
 export default Label;
