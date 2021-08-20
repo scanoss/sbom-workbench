4d7677f4bc66f99e16d6b337db361dcc71d2f440
Cancu <juancruz.cancu@gmail.com>
2021-08-20 16:06:10 -0300

    it seems it works

--- src/renderer/workbench/components/CodeEditor/CodeEditor.tsx
+++ src/renderer/workbench/components/CodeEditor/CodeEditor.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { nord } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import { range } from '../../../../utils/utils';
 
 interface CodeEditorProps {
   content: string;
@@ -8,17 +9,20 @@ interface CodeEditorProps {
 }
 
 const CodeEditor = ({ content, highlight }: CodeEditorProps) => {
+  const lines = range(parseInt(highlight?.split('-')[0], 10), parseInt(highlight?.split('-')[1], 10));
+
   return (
     <>
       <SyntaxHighlighter
-        className='code-viewer'
+        className="code-viewer"
         wrapLongLines
         style={nord}
-        language='javascript'
+        language="javascript"
         showLineNumbers
+        startingLineNumber={lines[99]}
         lineProps={(line) => {
           const style = { display: 'block', backgroundColor: 'inherit' };
-          if (highlight && highlight.includes(line)) {
+          if (lines && lines.includes(line)) {
             style.backgroundColor = '#ebe92252';
           }
           return { style };
@@ -28,8 +32,6 @@ const CodeEditor = ({ content, highlight }: CodeEditorProps) => {
       </SyntaxHighlighter>
     </>
   );
-}
+};
 
 export default CodeEditor;
-
-
