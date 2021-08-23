ff8f4953fb300fdb1005a6c17fed5d6e73725067
Cancu <juancruz.cancu@gmail.com>
2021-08-23 00:53:38 -0300

    casi scroll

--- src/renderer/workbench/components/CodeEditor/CodeEditor.tsx
+++ src/renderer/workbench/components/CodeEditor/CodeEditor.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { nord } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import { range } from '../../../../utils/utils';
@@ -11,6 +11,23 @@ interface CodeEditorProps {
 const CodeEditor = ({ content, highlight }: CodeEditorProps) => {
   const lines = range(parseInt(highlight?.split('-')[0], 10), parseInt(highlight?.split('-')[1], 10));
   console.log(lines);
+  const codeEditor = React.createRef();
+
+  const automaticScrollHandler = async () => {
+    const arrayFromHTMLCollection = document.getElementsByClassName('language-javascript');
+    const arraysito = Array.from(arrayFromHTMLCollection);
+    await arraysito.forEach((element) => {
+      element.children[lines[0]].setAttribute('id', 'linelaited');
+    });
+    const element = document.getElementById('linelaited');
+    element?.scrollIntoView({ behavior: 'smooth' })
+    // document.getElementsByClassName('language-javascript')[1].children[lines[0]].setAttribute('id', 'linelaited');
+  };
+
+  useEffect(() => {
+    automaticScrollHandler();
+  }, [codeEditor]);
+
   return (
     <>
       <SyntaxHighlighter
@@ -19,7 +36,6 @@ const CodeEditor = ({ content, highlight }: CodeEditorProps) => {
         style={nord}
         language="javascript"
         showLineNumbers
-        startingLineNumber={lines[99]}
         lineProps={(line) => {
           const style = { display: 'block', backgroundColor: 'inherit' };
           if (lines && lines.includes(line)) {
@@ -27,6 +43,10 @@ const CodeEditor = ({ content, highlight }: CodeEditorProps) => {
           }
           return { style };
         }}
+        onScroll={() => {
+          console.log('bananaaa');
+        }}
+        ref={codeEditor}
       >
         {content.slice(0, 30000)}
       </SyntaxHighlighter>
