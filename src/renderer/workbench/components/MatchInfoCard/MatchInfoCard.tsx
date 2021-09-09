c732499b14a8148634d2feff11f335d737efa257
Franco Stramana <franco.stramana@gmail.com>
2021-09-09 12:33:02 -0300

    DT-271  add scrollbar to component match view
    * Fix component match view repeating
    
    * fix scroll hidden on popover
    
    * offset x y on popover
    
    Co-authored-by: Agustin Groh <agusgroh@gmail.com>

--- src/renderer/workbench/components/MatchInfoCard/MatchInfoCard.tsx
+++ src/renderer/workbench/components/MatchInfoCard/MatchInfoCard.tsx
@@ -33,13 +33,24 @@ interface MatchInfoCardProps {
 
 const MatchInfoCard = ({ match, onSelect, status, selected, onAction }: MatchInfoCardProps) => {
   const [over, setOver] = useState<boolean>(false);
+  const [pos, setPos] = useState({ x: 0, y: 0 });
+  const ref = React.useRef();
+  const refParent = document.querySelector('#editor .content');
+
+  const handlerOpen = (e) => {
+    const x = ref.current.getBoundingClientRect().left - refParent.getBoundingClientRect().left;
+    const y = ref.current.getBoundingClientRect().top - refParent.getBoundingClientRect().top + 62;
+
+    setPos({ x, y });
+    setOver(true);
+  };
 
   return (
     <>
-      <article className="match-info-card-container">
+      <article className="match-info-card-container" ref={ref}>
         <div onClick={onSelect} style={selected ? { border: '#60A5FA 2px solid' } : {}} className="match-info-card">
           <div className={`match-info-card-content status-${status}`}>
-            <div onMouseEnter={() => setOver(true)} onMouseLeave={() => setOver(false)} className="label-info-div">
+            <div onMouseEnter={handlerOpen} onMouseLeave={() => setOver(false)} className="label-info-div">
               <img alt="component logo" className="logo-match-info-card" src={componentDefault} />
               <span className="component-span">{match.component}</span>
               <span className="version-span">{match.version}</span>
@@ -50,32 +61,6 @@ const MatchInfoCard = ({ match, onSelect, status, selected, onAction }: MatchInf
                   <span className="usage-label">USAGE</span>
                 )}
                 <span className="usage-value">{match.usage}</span>
-
-                {over ? (
-                  <div className="popover-container">
-                    <div className="component-details-matchinfo">
-                      <div className="tiny-container-detail-matchinfo">
-                        <p className="title-detail-matchinfo">License</p>
-                        <p className="desc-detail-matchinfo">{match?.license}</p>
-                      </div>
-                      <div className="tiny-container-detail-matchinfo">
-                        <p className="title-detail-matchinfo">PURL</p>
-                        <p className="desc-detail-matchinfo">{match?.purl}</p>
-                      </div>
-                      <div className="tiny-container-detail-matchinfo">
-                        <p className="title-detail-matchinfo">URL</p>
-                        <a
-                          href={match?.url}
-                          target="_blank"
-                          className="desc-detail-matchinfo url-matchinfo"
-                          rel="noreferrer"
-                        >
-                          {match?.url}
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                ) : null}
               </div>
             </div>
             <div className="match-info-card-buttons">
@@ -106,7 +91,7 @@ const MatchInfoCard = ({ match, onSelect, status, selected, onAction }: MatchInf
                 <>
                   <Tooltip title="Remove identification">
                     <IconButton onClick={() => onAction(MATCH_INFO_CARD_ACTIONS.ACTION_DETACH)}>
-                      <RestoreOutlined className="icon"/>
+                      <RestoreOutlined className="icon" />
                     </IconButton>
                   </Tooltip>
                   <Tooltip title="View identification">
@@ -119,6 +104,32 @@ const MatchInfoCard = ({ match, onSelect, status, selected, onAction }: MatchInf
             </div>
           </div>
         </div>
+
+        {over && (
+          <div
+            className="popover-container"
+            style={{ top: pos.y, left: pos.x }}
+            onMouseEnter={() => setOver(true)}
+            onMouseLeave={() => setOver(false)}
+          >
+            <div className="component-details-matchinfo">
+              <div className="tiny-container-detail-matchinfo">
+                <p className="title-detail-matchinfo">License</p>
+                <p className="desc-detail-matchinfo">{match?.license}</p>
+              </div>
+              <div className="tiny-container-detail-matchinfo">
+                <p className="title-detail-matchinfo">PURL</p>
+                <p className="desc-detail-matchinfo">{match?.purl}</p>
+              </div>
+              <div className="tiny-container-detail-matchinfo">
+                <p className="title-detail-matchinfo">URL</p>
+                <a href={match?.url} target="_blank" className="desc-detail-matchinfo url-matchinfo" rel="noreferrer">
+                  {match?.url}
+                </a>
+              </div>
+            </div>
+          </div>
+        )}
       </article>
     </>
   );
