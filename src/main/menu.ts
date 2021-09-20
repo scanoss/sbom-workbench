d77aeaf17ac8f55d398aa7951c53be9bd9cc44f8
franco.stramana <franco.stramana@gmail.com>
2021-09-20 11:20:20 -0300

    add edit menu to support copy and paste commands

--- src/main/menu.ts
+++ src/main/menu.ts
@@ -187,6 +187,20 @@ export default class MenuBuilder {
         ],
       },
       {
+        label: 'Edit',
+        submenu: [
+          /* {role: 'undo'},
+          {role: 'redo'},
+          {type: 'separator'}, */
+          {role: 'cut'},
+          {role: 'copy'},
+          {role: 'paste'},
+          /* {role: 'pasteandmatchstyle'},
+          {role: 'delete'},
+          {role: 'selectall'} */
+        ]
+      },
+      {
         label: '&View',
         submenu:
           process.env.NODE_ENV === 'development' ||
