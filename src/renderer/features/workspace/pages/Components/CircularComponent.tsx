216aaed718f8a2cde4a1259973116ce76331e382
Cancu <juancruz.cancu@gmail.com>
2021-09-30 15:26:26 -0300

    primary color varaibel added

--- src/renderer/features/workspace/pages/Components/CircularComponent.tsx
+++ src/renderer/features/workspace/pages/Components/CircularComponent.tsx
@@ -1,4 +1,5 @@
 import { Box, CircularProgress, IconButton, Typography } from '@material-ui/core';
+import { grey } from "@material-ui/core/colors";
 import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core';
 import PauseIcon from '@material-ui/icons/Pause';
@@ -11,6 +12,67 @@ interface CircularComponentProps {
   pauseScan: () => void;
 }
 
+const trackColor = '#D4D4D8';
+const textColor = '#71717A';  
+const backgroundColor = '#fefffe';
+
+const useStyles = makeStyles((theme) => ({
+  parentBox: {
+    position: 'relative',
+  },
+  circleParentBox: {
+    display: 'flex',
+    alignItems: 'center',  
+    justifyContent: 'center', 
+  },
+  trackCircularProgress: {
+    color: trackColor,
+    background: backgroundColor,
+    borderRadius: '50%',
+  },
+  circularProgress: {
+    color: theme.palette.primary.main,
+    position: 'absolute',
+  },
+  typographyContainer: {
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    position: 'absolute',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+  },
+  numberStageContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    marginTop: '50px',
+  },
+  number: {
+    fontSize: '4em',
+    color: theme.palette.primary.main,
+    fontWeight: 'bold',
+  },
+  stage: {
+    color: textColor,
+    fontSize: '1em',
+  },
+  pause: {
+    color: textColor,
+    fontSize: '0.75em',
+    zIndex: 5,
+  },
+  pauseContainer: {
+    marginTop: '20px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+}));
 
 const CircularComponent = ({stage, progress, pauseScan}: CircularComponentProps) => {
 
@@ -59,63 +121,9 @@ const CircularComponent = ({stage, progress, pauseScan}: CircularComponentProps)
     )
 }
 
-const useStyles = makeStyles({
-  parentBox: {
-    position: 'relative',
-  },
-  circleParentBox: {
-    display: 'flex',
-    alignItems: 'center',  
-    justifyContent: 'center', 
-  },
-  trackCircularProgress: {
-    color: '#d5d2d8',
-    background: '#fefffe',
-    borderRadius: '50%',
-  },
-  circularProgress: {
-    color: '#7D01F7',
-    position: 'absolute',
-  },
-  typographyContainer: {
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
-    position: 'absolute',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'column',
-  },
-  numberStageContainer: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'column',
-    marginTop: '50px',
-  },
-  number: {
-    fontSize: '4em',
-    color: '#7D01F7',
-    fontWeight: 'bold',
-  },
-  stage: {
-    color: '#71717A',
-    fontSize: '1em',
-  },
-  pause: {
-    color: '#71717A',
-    fontSize: '0.75em',
-    zIndex: 5,
-  },
-  pauseContainer: {
-    marginTop: '20px',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
+
+
+
 
 
 export default CircularComponent
