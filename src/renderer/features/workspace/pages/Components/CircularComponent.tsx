a393269e4b7debbfaae5ad579d000082027b5183
franco.stramana <franco.stramana@gmail.com>
2021-10-01 13:16:52 -0300

    fix circular loader

--- src/renderer/features/workspace/pages/Components/CircularComponent.tsx
+++ src/renderer/features/workspace/pages/Components/CircularComponent.tsx
@@ -1,29 +1,28 @@
-import { Box, CircularProgress, IconButton, Typography } from '@material-ui/core';
-import { grey } from "@material-ui/core/colors";
+import { Box, CircularProgress, IconButton, Typography, makeStyles, Button } from '@material-ui/core';
+import { grey } from '@material-ui/core/colors';
 import React, { useEffect, useState } from 'react';
-import { makeStyles } from '@material-ui/core';
+
 import PauseIcon from '@material-ui/icons/Pause';
 
-
-
 interface CircularComponentProps {
-  stage: String;
-  progress: Number;
+  stage: string;
+  progress: number;
   pauseScan: () => void;
 }
 
 const trackColor = '#D4D4D8';
-const textColor = '#71717A';  
+const textColor = '#71717A';
 const backgroundColor = '#fefffe';
 
 const useStyles = makeStyles((theme) => ({
   parentBox: {
     position: 'relative',
+    marginBottom: 60,
   },
   circleParentBox: {
     display: 'flex',
-    alignItems: 'center',  
-    justifyContent: 'center', 
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   trackCircularProgress: {
     color: trackColor,
@@ -63,7 +62,6 @@ const useStyles = makeStyles((theme) => ({
   },
   pause: {
     color: textColor,
-    fontSize: '0.75em',
     zIndex: 5,
   },
   pauseContainer: {
@@ -74,56 +72,46 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const CircularComponent = ({stage, progress, pauseScan}: CircularComponentProps) => {
-
-  const variant = (stage === 'preparing' || stage === 'indexing') ? 'indeterminate' : 'determinate';
+const CircularComponent = ({ stage, progress, pauseScan }: CircularComponentProps) => {
+  const variant = stage === 'preparing' || stage === 'indexing' ? 'indeterminate' : 'determinate';
 
   const classes = useStyles();
 
-    return (
+  return (
     <div className={classes.parentBox}>
       <div className={classes.circleParentBox}>
-            <CircularProgress
-              variant="determinate"
-              className={classes.trackCircularProgress}
-              size={'400px'}
-              thickness={3}
-              value={100}
-            />
-          <CircularProgress
-            variant={variant}
-            size={'400px'}
-            thickness={3}
-            className={classes.circularProgress}
-            disableShrink
-            {...{value: progress}}
-            />
-        </div>
-    <div
-      className={classes.typographyContainer}
-    >
-      <div className={classes.numberStageContainer}>
-        <span className={classes.number}>
-          {Math.round(progress)}{variant === 'determinate' ? '%' : ''}
-        </span>
-        <span className={classes.stage}>
-          {stage.toUpperCase()}
-        </span>
+        <CircularProgress
+          variant="determinate"
+          className={classes.trackCircularProgress}
+          size="340px"
+          thickness={3}
+          value={100}
+        />
+        <CircularProgress
+          variant={variant}
+          size="340px"
+          thickness={3}
+          className={classes.circularProgress}
+          disableShrink
+          {...{ value: progress }}
+        />
       </div>
-      <div className={classes.pauseContainer}>
-        <IconButton onClick={() => pauseScan()}>
-          <PauseIcon />
-          <span className={classes.pause}>Pause</span>
-        </IconButton> 
+      <div className={classes.typographyContainer}>
+        <div className={classes.numberStageContainer}>
+          <span className={classes.number}>
+            {Math.round(progress)}
+            {variant === 'determinate' ? '%' : ''}
+          </span>
+          <span className={classes.stage}>{stage.toUpperCase()}</span>
+        </div>
+        <div className={classes.pauseContainer}>
+          <Button startIcon={<PauseIcon />} onClick={pauseScan}>
+            <span className={classes.pause}>PAUSE</span>
+          </Button>
+        </div>
       </div>
     </div>
-  </div>
-    )
-}
+  );
+};
 
-
-
-
-
-
-export default CircularComponent
+export default CircularComponent;
--- src/renderer/features/workspace/pages/NewProject/NewProject.tsx
+++ src/renderer/features/workspace/pages/NewProject/NewProject.tsx
@@ -1,9 +1,8 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import { Box, CircularProgress, IconButton, LinearProgress, Typography } from '@material-ui/core';
+import { IconButton } from '@material-ui/core';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
-import CloseIcon from '@material-ui/icons/Close';
-import { AppContext } from '../../../../context/AppProvider';
+import { AppContext, IAppContext } from '../../../../context/AppProvider';
 import * as controller from '../../../../home-controller';
 import { IpcEvents } from '../../../../../ipc-events';
 import { DialogContext } from '../../../../context/DialogProvider';
@@ -54,9 +53,7 @@ const NewProject = () => {
   };
 
   const handlerScannerError = async (_event, args) => {
-    console.log(args);
-    // alert(args);
-    const errorMessage = `An error occurred while scanning. Please try again`;//args.message;
+    const errorMessage = `An error occurred while scanning. Please try again`; // args.message;
     const { action } = await dialogCtrl.openConfirmDialog(
       `${errorMessage}`,
       {
@@ -64,18 +61,13 @@ const NewProject = () => {
         role: 'accept',
       },
       true
-      );
-      history.goBack();
+    );
+    history.goBack();
 
-
-    //ipcRenderer.send(IpcEvents.SCANNER_RESUME);
-
-
-  }
+    // ipcRenderer.send(IpcEvents.SCANNER_RESUME);
+  };
 
   const onPauseHandler = async () => {
-    console.log("Button pressed");
-
     const { action } = await dialogCtrl.openConfirmDialog(
       `Are you sure you want to stop the scanner?`,
       {
@@ -83,22 +75,18 @@ const NewProject = () => {
         role: 'accept',
       },
       false
-      );
-      if(action === 'ok') {
-        // Call to the service and stop scanner.
-        await projectService.stop();
-        history.goBack();
-      }
+    );
+    if (action === 'ok') {
+      await projectService.stop();
+      history.goBack();
+    }
 
-    //ipcRenderer.send(IpcEvents.PROJECT_STOP);
-
-  }
+    // ipcRenderer.send(IpcEvents.PROJECT_STOP);
+  };
 
   const handlerScannerFinish = (_event, args) => {
     if (args.success) {
-      // onShowScan(args.resultsPath);
-    } else {
-      // showError();
+      onShowScan(args.resultsPath);
     }
   };
 
@@ -132,7 +120,5 @@ const NewProject = () => {
     </>
   );
 };
-
-
 
 export default NewProject;
