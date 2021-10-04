78bd0e3f484fdde16b07e92c78048668ac78756b
Agustin Groh <agusgroh@gmail.com>
2021-10-04 08:09:36 -0300

    Modify query for get spdx information

--- src/main/export/format/SpdxLite.ts
+++ src/main/export/format/SpdxLite.ts
@@ -4,7 +4,7 @@ import { Format } from '../Format';
 export class SpdxLite extends Format {
   constructor() {
     super();
-    this.extension = '.SPDXLite.spdx';
+    this.extension = 'SPDXLite.spdx';
   }
 
   // @override
@@ -15,11 +15,11 @@ export class SpdxLite extends Format {
     for (let i = 0; i < data.length; i += 1) {
       const bodyTemplate = `PackageName: ${data[i].name}\nPackageVersion: ${
         data[i].version
-      }\nPackageFileName:-\nPackageDownloadLocation: NONE\nFilesAnalyzed: false\nPackageHomePage: ${
+      }\nPackageFileName:-\nPackageDownloadLocation: ${data[i].purl}\nFilesAnalyzed: false\nPackageHomePage: ${
         data[i].url
       }\nConcludedLicense: ${data[i].license_name}\nPackageLicenseInfoFromFiles: ${
         data[i].license_name
-      }\nDeclaredLicense: ${data[i].license_name}\nCommentsonLicense:${
+      }\nDeclaredLicense: ${data[i].declareLicense}\nCommentsonLicense:${
         data[i].notes !== 'n/a' ? data[i].notes : '-'
       }\nCopyrightText: - \n\r`;
 
@@ -32,7 +32,7 @@ export class SpdxLite extends Format {
   private static templateHeader() {
     const template = `DocumentName: ${workspace
       .getOpenedProjects()[0]
-      .getProjectName()}\nDocumentNamespace: https://example.com/example-v1.0\nCreator: Person: 'Tool: SCANOSS Inventory Engine', 'Organization: http://scanoss.com'\n\r`;
+      .getProjectName()}\nDocumentNamespace: https://example.com/example-v1.0\nCreator: Person: SCANOSS Inventory Engine', 'Organization: http://scanoss.com'\n\r`;
     return template;
   }
 }
