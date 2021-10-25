c0e2139bacf6cafff793a6c3f516da333ec0cc8d
Agustin <isasmendi.agus@gmail.com>
2021-10-25 10:05:48 -0300

    updated licensesText

--- src/renderer/features/about/LicensesText.tsx
+++ src/renderer/features/about/LicensesText.tsx
@@ -62,6 +62,27 @@ const LicensesText = () => {
         SOFTWARE.</p>
       </div>
       <div className="item">
+        <h3>Abort-Controller</h3>
+        <h5>pkg:github/mysticatea/abort-controller@3.0.0</h5>
+        <h5>Copyright (c) 2017 Toru Nagashima</h5>
+        <h4>License: MIT</h4>
+        <p>Permission is hereby granted, free of charge, to any person obtaining a copy
+        of this software and associated documentation files (the "Software"), to deal
+        in the Software without restriction, including without limitation the rights
+        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+        copies of the Software, and to permit persons to whom the Software is
+        furnished to do so, subject to the following conditions:</p>
+        <p>The above copyright notice and this permission notice shall be included in all
+        copies or substantial portions of the Software.</p>
+        <p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        SOFTWARE.</p>
+      </div>
+      <div className="item">
           <h3>Chars.js</h3>
           <h5>pkg:github/chartjs/Char.js@v3.5.0</h5>
           <h5>Copyright (c) 2014-2021 Chart.js Contributors</h5>
@@ -104,7 +125,7 @@ const LicensesText = () => {
           SOFTWARE.</p>
       </div>
       <div className="item">
-          <h3>Electron-builder</h3>
+          <h3>Electron-debug</h3>
           <h5>pgk:github/sindresorhus/electron-debug@3.1.0</h5>
           <h5>Copyright (c) Sindre Sorhus sindresorhus@gmail.com (https://sindresorhus.com).</h5>
           <h4>License: MIT</h4>
@@ -125,9 +146,30 @@ const LicensesText = () => {
           SOFTWARE.</p>
       </div>
       <div className="item">
-          <h3>Electron-debug</h3>
-          <h5>pgk:github/smegahertz/electron-log@4.2.4</h5>
+          <h3>Electron-log</h3>
+          <h5>pgk:github/smegahertz/electron-log@4.4.1</h5>
           <h5>Copyright (c) 2016 Alexey Prokhorov</h5>
+          <h4>License: MIT</h4>
+          <p>Permission is hereby granted, free of charge, to any person obtaining a copy
+          of this software and associated documentation files (the "Software"), to deal
+          in the Software without restriction, including without limitation the rights
+          to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+          copies of the Software, and to permit persons to whom the Software is
+          furnished to do so, subject to the following conditions:</p>
+          <p>The above copyright notice and this permission notice shall be included in all
+          copies or substantial portions of the Software.</p>
+          <p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+          IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+          FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+          AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+          LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+          OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+          SOFTWARE.</p>
+      </div>
+      <div className="item">
+          <h3>Electron-updater</h3>
+          <h5>pgk:github/electron-userland/electron-builder@4.3.4</h5>
+          <h5>Copyright (c) 2015 Loopline Systems</h5>
           <h4>License: MIT</h4>
           <p>Permission is hereby granted, free of charge, to any person obtaining a copy
           of this software and associated documentation files (the "Software"), to deal
@@ -406,6 +448,27 @@ const LicensesText = () => {
           <h3>source-map-support</h3>
           <h5>pkg:github/evanw/node-source-map-support@0.5.19</h5>
           <h5>Copyright (c) 2014 Evan Wallace</h5>
+          <h4>License: MIT</h4>
+          <p>Permission is hereby granted, free of charge, to any person obtaining a copy
+          of this software and associated documentation files (the "Software"), to deal
+          in the Software without restriction, including without limitation the rights
+          to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+          copies of the Software, and to permit persons to whom the Software is
+          furnished to do so, subject to the following conditions:</p>
+          <p>The above copyright notice and this permission notice shall be included in all
+          copies or substantial portions of the Software.</p>
+          <p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+          IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+          FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+          AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+          LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+          OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+          SOFTWARE.</p>
+      </div>
+      <div className="item">
+          <h3>uuid</h3>
+          <h5>pkg:github/uuidjs/uuid@8.3.2</h5>
+          <h5>Copyright (c) 2010-2020 Robert Kieffer and other contributors</h5>
           <h4>License: MIT</h4>
           <p>Permission is hereby granted, free of charge, to any person obtaining a copy
           of this software and associated documentation files (the "Software"), to deal
