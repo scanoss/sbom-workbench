666638898d025f2fc4c0f5a6d650021aa2107746
Agustin Groh <agusgroh@gmail.com>
2021-11-02 06:36:43 -0300

    Removing logs

--- src/main/workspace/Project.ts
+++ src/main/workspace/Project.ts
@@ -68,7 +68,6 @@ export class Project extends EventEmitter {
   }
 
   public async upgrade() {
-    console.log('upgrading project');
     if (this.metadata.getVersion() === '11.4.9') {
       this.metadata.setAppVersion('0.8.0');
       this.metadata.save();
@@ -77,7 +76,6 @@ export class Project extends EventEmitter {
     const newVersion: string = await pMigration.up();
     this.metadata = await Metadata.readFromPath(this.metadata.getMyPath());
     this.metadata.setAppVersion(newVersion);
-    console.log(newVersion);
     this.metadata.save();
   }
 
