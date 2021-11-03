f0d7b3ff19d46865c788e0cbd4df2972a8ff9d58
franco.stramana <franco.stramana@gmail.com>
2021-11-03 12:42:57 -0300

    fix update node tree

--- src/main/inventory.ts
+++ src/main/inventory.ts
@@ -49,9 +49,8 @@ ipcMain.handle(IpcEvents.INVENTORY_ATTACH_FILE, async (event, arg: Partial<Inven
 
 ipcMain.handle(IpcEvents.INVENTORY_DETACH_FILE, async (event, inv: Partial<Inventory>) => {
   try {
-    const success: boolean = await logicInventoryService.detach(inv);
     const project = workspace.getOpenedProjects()[0];
-    const result = await project.scans_db.results.getNotOriginal(arg.files);
+    const result = await project.scans_db.results.getNotOriginal(inv.files);
     if (result !== undefined) {
       const node = project.getNodeFromPath(result.file_path);
       if (result.source === 'filtered') {
@@ -63,6 +62,8 @@ ipcMain.handle(IpcEvents.INVENTORY_DETACH_FILE, async (event, inv: Partial<Inven
       }
       project.save();
     }
+
+    const success: boolean = await logicInventoryService.detach(inv);
     return { status: 'ok', message: 'File detached to inventory successfully', success };
   } catch (e) {
     console.log('Catch an error on inventory: ', e);
