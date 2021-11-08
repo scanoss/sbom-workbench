9b1b478de1dda329655a9a38a9f3d348c6ab29d0
Cancu <juancruz.cancu@gmail.com>
2021-11-08 11:39:29 -0300

    svgs added and cdns deleted

--- src/renderer/features/workbench/components/FileTree/FileTree.tsx
+++ src/renderer/features/workbench/components/FileTree/FileTree.tsx
@@ -2,6 +2,11 @@ import React, { useContext, useState } from 'react';
 import CheckboxTree, { OnCheckNode } from 'react-checkbox-tree';
 import { useHistory } from 'react-router-dom';
 import { IWorkbenchContext, WorkbenchContext } from '../../store';
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import FolderOpenOutlined from '@material-ui/icons/FolderOpenOutlined';
+import SvgIcon from '@material-ui/core/SvgIcon';
+import InsertDriveFileOutlined from '@material-ui/icons/InsertDriveFileOutlined';
 
 export const FileTree = () => {
   const history = useHistory();
@@ -12,11 +17,15 @@ export const FileTree = () => {
 
   const aTree = [tree];
 
-  const [expanded, setExpanded] = useState<string[]>([aTree && aTree[0] ? aTree[0].value : '']);
+  const [expanded, setExpanded] = useState<string[]>([
+    aTree && aTree[0] ? aTree[0].value : '',
+  ]);
 
   const onSelectFile = ({ children, value }: OnCheckNode) => {
     if (!children) {
-      history.push(`/workbench/detected/file?path=${encodeURIComponent(value)}`);
+      history.push(
+        `/workbench/detected/file?path=${encodeURIComponent(value)}`
+      );
     }
   };
 
@@ -29,6 +38,17 @@ export const FileTree = () => {
           onClick={(targetNode) => onSelectFile(targetNode)}
           expandOnClick
           onExpand={(expandedItems) => setExpanded(expandedItems)}
+          icons={{
+            expandClose: <KeyboardArrowRightIcon />,
+            expandOpen: <KeyboardArrowDownIcon />,
+            parentClose: (
+              <SvgIcon component="object">
+                <img src={'./imgs/file.svg'} alt="" />
+              </SvgIcon>
+            ),
+            parentOpen: <FolderOpenOutlined />,
+            leaf: <InsertDriveFileOutlined />,
+          }}
         />
       ) : (
         <div className="loader">
