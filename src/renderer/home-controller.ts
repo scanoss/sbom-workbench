62f8a0edce314e725f6db565abc5930784eadd12
Agustin <isasmendi.agus@gmail.com>
2021-12-06 15:09:01 -0300

    deleted hardcoded project name

--- src/renderer/home-controller.ts
+++ src/renderer/home-controller.ts
@@ -3,15 +3,16 @@ import { INewProject } from '../api/types';
 import { workspaceService } from '../api/workspace-service';
 import { IpcEvents } from '../ipc-events';
 import { workspace } from '../main/workspace/Workspace';
+import path from 'path';
 
 const { ipcRenderer } = require('electron');
 
-export const scan = (path: string) => {
+export const scan = (dir: string) => {
 
   const pInfo: INewProject = {
-    name: 'myProject',
-    scan_root: path,
-    default_license: 'APACHE-2.0',
+    name: path.basename(dir),
+    scan_root: dir,
+    default_license: null,
   };
 
   workspaceService.createProject(pInfo);
