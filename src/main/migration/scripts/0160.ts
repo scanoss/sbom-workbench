2eb2f2ffc1dede3090a51e67de48f60434e190fe
Agustin Groh <agusgroh@gmail.com>
2021-12-10 08:56:01 -0300

    Refactor migration 0160

--- src/main/migration/scripts/0160.ts
+++ src/main/migration/scripts/0160.ts
@@ -1,21 +1,14 @@
 import fs from 'fs';
-import sqlite3 from 'sqlite3';
 import log from 'electron-log';
-import { NodeStatus } from '../../workspace/Tree/Tree/Node';
 import { Tree } from '../../workspace/Tree/Tree/Tree';
-import { ChildCare } from '@material-ui/icons';
-
 
 function updateRecurTree(root) {
-
-  for(const child of root.children) {
-
-    if(child.type === 'folder') {
+  for (const child of root.children) {
+    if (child.type === 'folder') {
       updateRecurTree(child);
       child.updateStatusFlags();
     }
   }
-
 }
 
 export async function flagTreeFolderMigration(projectPath: string) {
@@ -26,53 +19,7 @@ export async function flagTreeFolderMigration(projectPath: string) {
   newTree.loadTree(project.tree.rootFolder);
 
   updateRecurTree(newTree.getRootFolder());
-
-  console.log(newTree.getRootFolder());
-  // const results: any = await getResults(projectPath);
-  // newTree.sync(results);
   project.tree = newTree;
 
   fs.writeFileSync(`${projectPath}/tree.json`, JSON.stringify(project, null, 2), 'utf8');
-}
-
-async function getResults(projectPath) {
-  return new Promise((resolve, reject) => {
-    const db: any = new sqlite3.Database(`${projectPath}/scan_db`, sqlite3.OPEN_READWRITE, (err: any) => {
-      if (err) {
-        log.error(err);
-      }
-      db.run('PRAGMA journal_mode = WAL;');
-      db.run('PRAGMA synchronous = OFF');
-      db.run('PRAGMA foreign_keys = ON;');
-      db.all(
-        `SELECT r.idtype,r.file_path as path,r.identified ,r.ignored ,(CASE WHEN  r.identified=0 AND r.ignored=0 THEN 1 ELSE 0 END) as pending FROM results r;`,
-        (err: any, results: any) => {
-          results.forEach((result) => {
-            if (result.idtype === 'none' && result.identified === 1) {
-              result[result.path] = NodeStatus.IDENTIFIED;
-              result.status = NodeStatus.IDENTIFIED;
-              result.original = NodeStatus.NOMATCH;
-            } else if (result.idtype === 'none') {
-              result[result.path] = NodeStatus.NOMATCH;
-              result.status = NodeStatus.NOMATCH;
-              result.original = NodeStatus.NOMATCH;
-            } else if (result.identified === 1) {
-              result[result.path] = NodeStatus.IDENTIFIED;
-              result.status = NodeStatus.IDENTIFIED;
-              result.original = NodeStatus.MATCH;
-            } else if (result.ignored === 1) {
-              result[result.path] = NodeStatus.IGNORED;
-              result.status = NodeStatus.IGNORED;
-              result.original = NodeStatus.MATCH;
-            } else if (result.pending === 1) {
-              result[result.path] = NodeStatus.PENDING;
-              result.status = NodeStatus.PENDING;
-              result.original = NodeStatus.MATCH;
-            }
-          });
-          resolve(results);
-        }
-      );
-    });
-  });
 }
