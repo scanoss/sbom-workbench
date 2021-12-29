f89c966eb802c3e98266a19240ef6c87cac0d147
Cancu <52618089+Cancuuu@users.noreply.github.com>
2021-12-29 15:48:33 -0300

    Fixs UI details on preload inventory dialog
    * details 448 done
    
    * fix shrink width
    
    * spinner while fetch and tooltip added
    
    * fix message ui
    
    * adds linter suggestions
    
    Co-authored-by: franco.stramana <franco.stramana@gmail.com>

--- src/renderer/ui/dialog/PreLoadInventoryDialog.tsx
+++ src/renderer/ui/dialog/PreLoadInventoryDialog.tsx
@@ -10,15 +10,13 @@ import {
   Button,
   Paper,
   FormControlLabel,
+  TextareaAutosize,
+  Tooltip,
+  CircularProgress,
 } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
 import { inventoryService } from '../../../api/inventory-service';
 import componentDefault from '../../../../assets/imgs/component-default.svg';
-
-
-
-// or
-
 
 const useStyles = makeStyles((theme) => ({
   size: {
@@ -58,12 +56,8 @@ const useStyles = makeStyles((theme) => ({
   },
   listItem: {
     padding: '0px',
-    margin: '24px 0px',
-    height: '25px',
+    margin: '0px',
   },
-  icon: {
-    minWidth: '0px !important',
-  }
 }));
 
 interface IPreLoadInventoryDialog {
@@ -78,7 +72,7 @@ export const PreLoadInventoryDialog = (props: IPreLoadInventoryDialog) => {
   const { open, folder, overwrite, onClose, onCancel } = props;
   const classes = useStyles();
 
-  const [inventories, setInventories] = useState<any[]>([]);
+  const [inventories, setInventories] = useState<any[]>(null);
 
   const [checked, setChecked] = useState<any[]>([]);
 
@@ -106,13 +100,13 @@ export const PreLoadInventoryDialog = (props: IPreLoadInventoryDialog) => {
   };
 
   const selectAll = () => {
-    if (checked.length === inventories.length) {
+    if (checked.length === inventories?.length) {
       setChecked([]);
     } else setChecked(inventories);
   };
 
   const AllChecked = () => {
-    return checked.length === inventories.length;
+    return checked.length === inventories?.length;
   };
 
   const init = async () => {
@@ -128,7 +122,7 @@ export const PreLoadInventoryDialog = (props: IPreLoadInventoryDialog) => {
   useEffect(() => {
     if (open) init();
     else {
-      setInventories([]);
+      setInventories(null);
       setChecked([]);
       setNotes('');
     }
@@ -142,67 +136,96 @@ export const PreLoadInventoryDialog = (props: IPreLoadInventoryDialog) => {
           <div>
             <List>
               <FormControlLabel control={<Checkbox checked={AllChecked()} onClick={() => selectAll()} />} label="All" />
-              <hr className='divider' />
-              <div className='scroll-list'>
-              {inventories.map((value) => {
-                const labelId = `checkbox-list-label-${value.cvid}`;
-                return (
-                  <ListItem className={classes.listItem} key={value}>
-                    <ListItemIcon className={classes.icon} role={undefined} onClick={handleToggle(value)}>
-                      <Checkbox
-                        edge="start"
-                        checked={
-                          checked.findIndex(
-                            (x) =>
-                              x.version === value.version &&
-                              x.purl === value.purl &&
-                              x.spdxid === value.spdxid &&
-                              x.usage === value.usage
-                          ) !== -1
-                        }
-                        tabIndex={-1}
-                        disableRipple
-                        inputProps={{ 'aria-labelledby': labelId }}
-                        className={classes.icon}
-                      />
-                    </ListItemIcon>
-                    <div className='checkbox-info'>
-                      <div className='name-component'>
-                      <svg width="25" height="22" viewBox="0 0 25 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M18.3333 9.58203V4.30391C18.3333 3.60078 17.8973 2.97266 17.2364 2.72422L12.9239 1.10703C12.5442 0.961719 12.1223 0.961719 11.7379 1.10703L7.42544 2.72422C6.7645 2.97266 6.32856 3.60078 6.32856 4.30391V9.58203L1.43013 11.4242C0.769189 11.668 0.333252 12.3008 0.333252 13.0039V18.0008C0.333252 18.6383 0.694189 19.2242 1.26606 19.5101L5.88794 21.8211C6.36138 22.0602 6.92388 22.0602 7.39731 21.8211L12.3333 19.3555L17.2692 21.8211C17.7426 22.0602 18.3051 22.0602 18.7786 21.8211L23.4004 19.5101C23.9723 19.2242 24.3333 18.6383 24.3333 18.0008V13.0039C24.3333 12.3008 23.8973 11.6727 23.2364 11.4242L18.3333 9.58203ZM12.8958 11.2133V6.62422L17.2083 5.13828V9.71328L12.8958 11.2133ZM7.45825 3.91484L12.3333 2.08672L17.2083 3.91484V3.92422L12.3333 5.63984L7.45825 3.91953V3.91484ZM7.45825 5.13828L11.7708 6.62422V11.2133L7.45825 9.71328V5.13828ZM6.052 20.5789L1.5145 18.3102V13.9367L6.052 15.7789V20.5789ZM1.5145 12.6617V12.6523L6.64263 10.7305L11.7098 12.6289V12.6852L6.64263 14.743L1.5145 12.6617ZM7.23325 15.7789L11.7098 13.9602V18.343L7.23325 20.5836V15.7789ZM17.4333 20.5789L12.9567 18.343V13.9648L17.4333 15.7836V20.5789ZM23.152 18.3102L18.6145 20.5789V15.7789L23.152 13.9367V18.3102ZM23.152 12.6617L18.0239 14.743L12.9567 12.6852V12.6289L18.0239 10.7305L23.152 12.6523V12.6617Z" fill="#3B82F6"/>
-                      </svg>
-                      <p className='list-item-text'>{`${value.purl}`}</p>
-                      </div>
-                      <div className='pills'>
-                        <div className='version-pill'>
-                          <p>{`${value.version}`.slice(0, 10).trim()}</p>
-                        </div>
-                        <div className='license-pill'>
-                          <p>{`${value.spdxid}`.slice(0, 10).trim()}</p>
-                        </div>
-                      </div>
-                    </div>
-                  </ListItem>
-                );
-              })}
+              <hr className="divider" />
+              <div className="scroll-list">
+                {inventories === null && (
+                  <div className="d-flex">
+                    <CircularProgress size={20} />{' '}
+                  </div>
+                )}
+                {inventories && inventories.length === 0 && <span>No componentes were detected</span>}
+                {inventories?.length > 0 &&
+                  inventories.map((value) => {
+                    const labelId = `checkbox-list-label-${value.cvid}`;
+                    return (
+                      <ListItem className={classes.listItem} key={value.cvid}>
+                        <ListItemIcon className="list-item" role={undefined} onClick={handleToggle(value)}>
+                          <Checkbox
+                            edge="start"
+                            checked={
+                              checked.findIndex(
+                                (x) =>
+                                  x.version === value.version &&
+                                  x.purl === value.purl &&
+                                  x.spdxid === value.spdxid &&
+                                  x.usage === value.usage
+                              ) !== -1
+                            }
+                            tabIndex={-1}
+                            disableRipple
+                            inputProps={{ 'aria-labelledby': labelId }}
+                          />
+                          <div className="checkbox-info">
+                            <div className="name-component">
+                              <svg
+                                width="25"
+                                height="22"
+                                viewBox="0 0 25 22"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                              >
+                                <path
+                                  d="M18.3333 9.58203V4.30391C18.3333 3.60078 17.8973 2.97266 17.2364 2.72422L12.9239 1.10703C12.5442 0.961719 12.1223 0.961719 11.7379 1.10703L7.42544 2.72422C6.7645 2.97266 6.32856 3.60078 6.32856 4.30391V9.58203L1.43013 11.4242C0.769189 11.668 0.333252 12.3008 0.333252 13.0039V18.0008C0.333252 18.6383 0.694189 19.2242 1.26606 19.5101L5.88794 21.8211C6.36138 22.0602 6.92388 22.0602 7.39731 21.8211L12.3333 19.3555L17.2692 21.8211C17.7426 22.0602 18.3051 22.0602 18.7786 21.8211L23.4004 19.5101C23.9723 19.2242 24.3333 18.6383 24.3333 18.0008V13.0039C24.3333 12.3008 23.8973 11.6727 23.2364 11.4242L18.3333 9.58203ZM12.8958 11.2133V6.62422L17.2083 5.13828V9.71328L12.8958 11.2133ZM7.45825 3.91484L12.3333 2.08672L17.2083 3.91484V3.92422L12.3333 5.63984L7.45825 3.91953V3.91484ZM7.45825 5.13828L11.7708 6.62422V11.2133L7.45825 9.71328V5.13828ZM6.052 20.5789L1.5145 18.3102V13.9367L6.052 15.7789V20.5789ZM1.5145 12.6617V12.6523L6.64263 10.7305L11.7098 12.6289V12.6852L6.64263 14.743L1.5145 12.6617ZM7.23325 15.7789L11.7098 13.9602V18.343L7.23325 20.5836V15.7789ZM17.4333 20.5789L12.9567 18.343V13.9648L17.4333 15.7836V20.5789ZM23.152 18.3102L18.6145 20.5789V15.7789L23.152 13.9367V18.3102ZM23.152 12.6617L18.0239 14.743L12.9567 12.6852V12.6289L18.0239 10.7305L23.152 12.6523V12.6617Z"
+                                  fill="#3B82F6"
+                                />
+                              </svg>
+                              <p className="list-item-text">{`${value.purl}`}</p>
+                            </div>
+                            <div className="pills">
+                              <div className="version-pill">
+                                <p>{value.version.trim() === '' ? '-' : `${value.version}`.slice(0, 10)}</p>
+                              </div>
+                              {value.spdxid.length > 10 ? (
+                                <Tooltip title={value.spdxid}>
+                                  <div className="license-pill">
+                                    <p>{value.spdxid.trim() === '' ? '-' : `${value.spdxid.slice(0, 10)}...`}</p>
+                                  </div>
+                                </Tooltip>
+                              ) : (
+                                <div className="license-pill">
+                                  <p>{value.spdxid.trim() === '' ? '-' : `${value.spdxid}`}</p>
+                                </div>
+                              )}
+                            </div>
+                          </div>
+                        </ListItemIcon>
+                      </ListItem>
+                    );
+                  })}
               </div>
             </List>
           </div>
-            <hr className='divider' />
+          <hr className="divider" />
           <div>
             <label className="dialog-form-field-label">
               Notes <span className="optional">(Optional)</span>
             </label>
-            <div className="dialog-form-field-control">
-              <textarea placeholder="Example" name="notes" value={notes || ''} cols={30} rows={5} onChange={(e) => inputHandler(e)} />
-            </div>
+            <Paper className="dialog-form-field-control">
+              <TextareaAutosize
+                placeholder="Example"
+                name="notes"
+                value={notes || ''}
+                cols={30}
+                rows={5}
+                onChange={(e) => inputHandler(e)}
+              />
+            </Paper>
           </div>
-          <div className='button-container'>
+          <div className="button-container">
             <form onSubmit={onSubmit}>
               <Button onClick={onCancel}>Cancel</Button>
               <Button type="submit" variant="contained" color="secondary" disabled={!isValid()}>
-                {' '}
-                Identify{' '}
+                Identify
               </Button>
             </form>
           </div>
