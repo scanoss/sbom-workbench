c8ebcea62a27371c78fcb25445390681d1c25d12
franco.stramana <franco.stramana@gmail.com>
2022-01-07 18:39:25 -0300

    Adds feature flag on component logo

--- src/renderer/features/workbench/components/IconComponent/IconComponent.tsx
+++ src/renderer/features/workbench/components/IconComponent/IconComponent.tsx
@@ -1,25 +1,30 @@
 import React from 'react';
 import componentDefault from '../../../../../../assets/imgs/component-default.svg';
+import { ENABLE_COMPONENT_LOGO } from '../../../../../Config';
 import style from './IconComponent.scss';
 
 const IconComponent = ({ name, size }) => {
   return (
     <div id={style.IconComponent}>
       <figure style={{ width: size, height: size }}>
-        <img
-          alt="component logo"
-          height={size}
-          loading="lazy"
-          style={{ backgroundImage: componentDefault }}
-          src={`https://avatars.githubusercontent.com/${name}?s=${size}}`}
-          onLoad={(event: any) => {
-            event.target.style.backgroundImage = 'none';
-          }}
-          onError={(event: any) => {
-            event.target.src = componentDefault;
-            event.onerror = null;
-          }}
-        />
+        {ENABLE_COMPONENT_LOGO ? (
+          <img
+            alt="component logo"
+            height={size}
+            loading="lazy"
+            style={{ backgroundImage: componentDefault }}
+            src={`https://avatars.githubusercontent.com/${name}?s=${size}}`}
+            onLoad={(event: any) => {
+              event.target.style.backgroundImage = 'none';
+            }}
+            onError={(event: any) => {
+              event.target.src = componentDefault;
+              event.onerror = null;
+            }}
+          />
+        ) : (
+          <img alt="logo" src={componentDefault} />
+        )}
       </figure>
     </div>
   );
