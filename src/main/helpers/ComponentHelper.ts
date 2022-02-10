a417edc331530a7f1d101f5be1665ff8ab953cfe
Agustin Groh <agusgroh@gmail.com>
2022-02-10 10:32:01 -0300

    Fixs bug when get manual components

--- src/main/helpers/ComponentHelper.ts
+++ src/main/helpers/ComponentHelper.ts
@@ -17,6 +17,51 @@ class ComponentHelper {
     });
     return components;
   }
+
+  public processComponent(data: any) {
+    const results: any = [];
+
+    for (let i = 0; i < data.length; i += 1) {
+      const transformation: any = {};
+      const preLicense: any = {};
+      transformation.compid = data[i].compid;
+      transformation.licenses = [];
+      transformation.name = data[i].comp_name;
+      transformation.purl = data[i].purl;
+      transformation.url = data[i].comp_url;
+      transformation.version = data[i].version;
+      transformation.vendor = data[i].vendor;
+      if (data[i].filesCount) transformation.filesCount = data[i].filesCount;
+
+      if (data[i].license_id) {
+        preLicense.id = data[i].license_id;
+        preLicense.name = data[i].license_name;
+        preLicense.spdxid = data[i].license_spdxid;
+        transformation.licenses.push(preLicense);
+      }
+      results.push(transformation);
+      let countMerged = 0;
+      for (let j = i + 1; j < data.length; j += 1) {
+        if (data[i].compid < data[j].compid) break;
+
+        if (data[i].compid === data[j].compid) {
+          this.mergeComponents(results[results.length - 1], data[j]);
+          countMerged += 1;
+        }
+      }
+      i += countMerged;
+    }
+    return results;
+  }
+
+  // merge component b into a
+  private mergeComponents(a: any, b: any) {
+    const preLicense: any = {};
+    preLicense.id = b.license_id;
+    preLicense.name = b.license_name;
+    preLicense.spdxid = b.license_spdxid;
+    a.licenses.push(preLicense);
+  }
 }
 
 export const componentHelper = new ComponentHelper();
