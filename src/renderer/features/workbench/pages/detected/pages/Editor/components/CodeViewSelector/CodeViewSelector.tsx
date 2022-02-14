9093c4de4571aea5b2f0c4ed4c9ca53640a70f14
Agustin Groh <agusgroh@gmail.com>
2022-02-14 11:56:34 -0300

    Adds tooltip on codeViewSelector

--- src/renderer/features/workbench/pages/detected/pages/Editor/components/CodeViewSelector/CodeViewSelector.tsx
+++ src/renderer/features/workbench/pages/detected/pages/Editor/components/CodeViewSelector/CodeViewSelector.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ButtonGroup, Button, makeStyles } from '@material-ui/core';
+import { ButtonGroup, Button, makeStyles, Tooltip } from '@material-ui/core';
 import AccountTreeOutlined from '@material-ui/icons/AccountTreeOutlined';
 import CodeOutlinedIcon from '@material-ui/icons/CodeOutlined';
 
@@ -19,22 +19,26 @@ const CodeViewSelector = ({ active, setView }) => {
   return (
     <div className={classes.root}>
       <ButtonGroup variant="outlined" size="small" aria-label="file view selector">
-        <Button
-          className={classes.button}
-          onClick={() => setView('code')}
-          color={active === 'code' ? 'primary' : 'default'}
-          aria-label="code"
-        >
-          <CodeOutlinedIcon fontSize="inherit" />
-        </Button>
-        <Button
-          className={classes.button}
-          onClick={() => setView('graph')}
-          color={active === 'graph' ? 'primary' : 'default'}
-          aria-label="graph"
-        >
-          <AccountTreeOutlined fontSize="inherit" />
-        </Button>
+        <Tooltip title="Raw view" arrow>
+          <Button
+            className={classes.button}
+            onClick={() => setView('code')}
+            color={active === 'code' ? 'primary' : 'default'}
+            aria-label="code"
+          >
+            <CodeOutlinedIcon fontSize="inherit" />
+          </Button>
+        </Tooltip>
+        <Tooltip title="Dependency view" arrow>
+          <Button
+            className={classes.button}
+            onClick={() => setView('graph')}
+            color={active === 'graph' ? 'primary' : 'default'}
+            aria-label="graph"
+          >
+            <AccountTreeOutlined fontSize="inherit" />
+          </Button>
+        </Tooltip>
       </ButtonGroup>
     </div>
   );
