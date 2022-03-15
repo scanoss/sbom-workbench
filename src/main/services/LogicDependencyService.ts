a2e56c437d4c372b10452aa1704bdbeff179c29c
Agustin Groh <77737320+agustingroh@users.noreply.github.com>
2022-03-15 14:11:19 +0100

    DT-589 Adds accept dependency service
    * DT-589-Adds-accept-dependency-service
    
    * Adds source attribute to inventoryModel
    
    * Removes queryBuilderSource

--- src/main/services/LogicDependencyService.ts
+++ src/main/services/LogicDependencyService.ts
@@ -1,7 +1,9 @@
+import log from 'electron-log';
 import { IDependencyResponse } from 'scanoss';
 import { DependencyDTO } from '../../api/types';
 import { dependencyHelper } from '../helpers/DependencyHelper';
 import { fileHelper } from '../helpers/FileHelper';
+import { licenseHelper } from '../helpers/LicenseHelper';
 import { QueryBuilderCreator } from '../queryBuilder/QueryBuilderCreator';
 import { serviceProvider } from './ServiceProvider';
 
@@ -13,9 +15,61 @@ class LogicDependencyService {
   }
 
   public async getAll(params: any): Promise<Array<DependencyDTO>> {
-    const queryBuilder = QueryBuilderCreator.create(params);
-    const dependencies = await serviceProvider.model.dependency.getAll(queryBuilder);
-    return dependencies;
+    try {
+      const queryBuilder = QueryBuilderCreator.create(params);
+      const dependencies = await serviceProvider.model.dependency.getAll(queryBuilder);
+      const inventory: any = await serviceProvider.model.inventory.getAll();
+      const component: any = await serviceProvider.model.component.getAll();
+      dependencyHelper.mergeInventoryComponentToDependency(dependencies, inventory, component);
+      return dependencies;
+    } catch (err: any) {
+      log.error(err);
+      return err;
+    }
+  }
+
+  public async accept(params: any): Promise<DependencyDTO> {
+    try {
+      if (!params.id) throw new Error('Dependency id is required');
+      const queryBuilderDependency = QueryBuilderCreator.create({ id: params.id });
+      let dependency: any = (await serviceProvider.model.dependency.getAll(queryBuilderDependency))[0];
+
+      const queryBuilerComp = QueryBuilderCreator.create({ purl: params.purl, version: params.version });
+      let comp = (await serviceProvider.model.component.getAll(queryBuilerComp))[0];
+      // Adds license to dependency if the user define one
+      if (dependency.licenses.length === 0) {
+        dependency = { ...dependency, licenses: params.license };
+        await serviceProvider.model.dependency.insertLicense(dependency);
+      }
+
+      let lic: any = await serviceProvider.model.license.getBySpdxId(params.license);
+      const licenseName = licenseHelper.licenseNameToSPDXID(params.license);
+      if (!lic)
+        lic = await serviceProvider.model.license.create({
+          spdxid: params.license,
+          name: licenseName,
+          fulltext: '',
+          url: '',
+        });
+
+      if (!comp)
+        comp = await serviceProvider.model.component.create({
+          name: dependency.componentName,
+          version: params.version,
+          purl: params.purl,
+          license_id: lic.id,
+        });
+      else {
+        await serviceProvider.model.license.licenseAttach({ license_id: lic.id, compid: comp.compid });
+      }
+      await serviceProvider.model.dependency.accept(dependency);
+      await serviceProvider.model.inventory.create({ cvid: comp.compid, spdxid: params.license, source: 'declared' });
+      const response = (await this.getAll({ id: params.id }))[0];
+      return response;
+    } catch (error: any) {
+      log.error(error);
+      return error;
+    }
   }
 }
 
