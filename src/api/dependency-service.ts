57475d2bb2156264e93ba6b08365e23d1db96f2a
Agustin Groh <77737320+agustingroh@users.noreply.github.com>
2022-03-15 14:11:19 +0100

    DT-589 Adds accept dependency service
    * DT-589-Adds-accept-dependency-service
    
    * Adds source attribute to inventoryModel
    
    * Removes queryBuilderSource

--- src/api/dependency-service.ts
+++ src/api/dependency-service.ts
@@ -1,11 +1,17 @@
 import { IpcEvents } from '../ipc-events';
 import { BaseService } from './base-service';
+import { DependencyDTO } from './types';
 
 const { ipcRenderer } = require('electron');
 
 class DepencyService extends BaseService {
   public async getAll(params: any): Promise<any> {
     const response = await ipcRenderer.invoke(IpcEvents.DEPENDENCY_GET_ALL, params);
+    return this.response(response);
+  }
+
+  public async accept(params: Partial<DependencyDTO>): Promise<DependencyDTO> {
+    const response = await ipcRenderer.invoke(IpcEvents.DEPENDENCY_ACCEPT, params);
     return this.response(response);
   }
 }
--- src/main/dependency.ts
+++ src/main/dependency.ts
@@ -12,3 +12,13 @@ ipcMain.handle(IpcEvents.DEPENDENCY_GET_ALL, async (event, params: any) => {
     return Response.fail({ message: error.message });
   }
 });
+
+ipcMain.handle(IpcEvents.DEPENDENCY_ACCEPT, async (event, params: any) => {
+  try {
+    const dependency = await logicDependencyService.accept(params);
+    return Response.ok({ message: 'Component created successfully', data: dependency });
+  } catch (error: any) {
+    console.log('Catch an error: ', error);
+    return Response.fail({ message: error.message });
+  }
+});
