6caddcd844805aef15e4e4b194017be86851b2b3
franco.stramana <franco.stramana@gmail.com>
2022-04-07 17:06:41 -0300

    DT-609 Implement feature flag about message

--- src/config/AppConfigDefault.ts
+++ src/config/AppConfigDefault.ts
@@ -3,6 +3,8 @@ import { IAppConfig } from './IAppConfig';
 
 export const AppConfigDefault: IAppConfig = {
   APP_NAME: 'SCANOSS Audit Workbench',
+  ABOUT_MESSAGE:
+    'SCANOSS Audit Workbench brings free of charge, secure and anonymous Open Source Auditing to your desktop.',
   API_URL: 'https://osskb.org/api',
   MIN_VERSION_SUPPORTED: '0.17.0',
   DEFAULT_WORKSPACE_NAME: 'scanoss-workspace',
