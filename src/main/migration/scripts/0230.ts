0be1895b6e6cc913b48e05b4f1b78acaf2f6a063
Agustin Groh <77737320+agustingroh@users.noreply.github.com>
2022-04-29 14:59:27 +0200

    Hotfix: Fixes bug on migration 023

--- src/main/migration/scripts/0230.ts
+++ src/main/migration/scripts/0230.ts
@@ -17,8 +17,9 @@ import {NodeStatus} from "../../workspace/Tree/Tree/Node";
 
 export async function migration0230(projectPath: string): Promise<void> {
   log.info("Migration 0230 In progress...");
+    await modelProvider.init(projectPath);
     await regenerateDependencyTable(projectPath);
-  await init(projectPath);
+    await addDependencies(projectPath);
 }
 
 
@@ -38,41 +39,39 @@ async function regenerateDependencyTable(projectPath): Promise<void> {
   });
 }
 
-
- async function  init(projectPath: string){
+ async function  addDependencies(projectPath: string){
   try {
-
-    const dependencies = JSON.parse(
-      await fs.promises.readFile(`${projectPath}/dependencies.json`, 'utf8'));
-
-    //import dependencies on file fileTree
-    await updateTree(projectPath,dependencies);
-
-
-    // import dependencies on DB
-    const filesDependencies = dependencyHelper.dependecyModelAdapter(dependencies);
-    const files = await getPathFileId(projectPath);
-    const filesIds = [];
-    filesDependencies.forEach((fileDependency) => {
-      fileDependency.fileId=files[fileDependency.file];
-      filesIds.push(files[fileDependency.file]);
-    });
-    await updateFileType(projectPath,filesIds,"MATCH");
-    await insert(projectPath,filesDependencies);
-
+    const depPath = `${projectPath}/dependencies.json`;
+    const exist = await fileHelper.fileExist(depPath);
+    if(exist){
+      const dependencies:any = JSON.parse(await fs.promises.readFile(`${depPath}`, 'utf8'));
+       await updateDependenciesOnTree(projectPath,dependencies);
+       await updateFlagsOnTree(projectPath);
+      // import dependencies on DB
+      await dependencyService.insert(dependencies);
+    }
   } catch (e) {
     log.error(e);
   }
 }
 
-async function updateTree(projectPath:string,dependencies){
+async function updateFlagsOnTree(projectPath:string){
+  const project = await fs.promises.readFile(`${projectPath}/tree.json`, 'utf8');
+  const a = JSON.parse(project);
+  const tree = new Tree(projectPath, null);
+  tree.loadTree(a.tree.rootFolder);
+  const rootFolder = tree.getRootFolder();
+  addProgressFlags(rootFolder);
+  a.tree  = tree;
+  await fs.promises.writeFile(`${projectPath}/tree.json`, JSON.stringify(a));
+}
+
+async function updateDependenciesOnTree(projectPath:string,dependencies){
   const project = await fs.promises.readFile(`${projectPath}/tree.json`, 'utf8');
   const a = JSON.parse(project);
   const tree = new Tree(projectPath, null);
   tree.loadTree(a.tree.rootFolder);
   tree.addDependencies(dependencies);
-  const rootFolder = tree.getRootFolder();
-  addProgressFlags(rootFolder);
   a.tree  = tree;
   await fs.promises.writeFile(`${projectPath}/tree.json`, JSON.stringify(a));
 }
@@ -82,13 +81,11 @@ function addProgressFlags(node) {
     node.hasIdentifiedProgress = false;
     node.hasPendingProgress = false;
     node.hasIgnoredProgress = false;
-
     node.hasPending = false;
     node.hasIdentified = false;
     node.hasIgnored = false;
     node.hasNoMatch = false;
     node.hasFiltered = false;
-
 
     for (const child of node.getChildren()) {
       addProgressFlags(child);
@@ -109,86 +106,8 @@ function addProgressFlags(node) {
 }
 
 
-async function getPathFileId(projectPath:string) {
-  const query = new Querys();
-  return new Promise((resolve, reject) => {
-    try {
-      const db: any = new sqlite3.Database(`${projectPath}/scan_db`, sqlite3.OPEN_READWRITE, (err: any) => {
-        if (err) log.error(err);
-        db.all("SELECT f.fileId AS id,f.type,f.path,f.identified,f.ignored,r.matched,r.idtype AS type,r.lines,r.oss_lines,r.file_url,fi.inventoryid, r.license, r.component AS componentName, r.url,comp.purl,comp.version\n  FROM files f LEFT JOIN results r ON r.fileId=f.fileId LEFT JOIN component_versions comp ON\n  comp.purl = r.purl AND comp.version = r.version\n  LEFT JOIN file_inventories fi ON fi.fileId=f.fileId",(err: any, files: any) => {
-          if (err) log.error(err);
-          db.close();
-          const pathFileId = files.reduce((acc, file) => {
-            acc[file.path] = file.id;
-            return acc;
-          }, {});
-          resolve(pathFileId);
-        });
-      });
-    } catch (e) {
-      reject(e);
-    }
-  });
-}
 
 
 
 
-async function  updateFileType(projectPath,fileIds: number[], fileType: string) {
-  return new Promise<void>(async (resolve, reject) => {
-    try {
-      const db: any = new sqlite3.Database(`${projectPath}/scan_db`, sqlite3.OPEN_READWRITE, (err: any) => {
-        const SQLquery = `UPDATE files SET type=? WHERE fileId IN (${fileIds.toString()});`;
-        db.run(SQLquery, fileType, (err: any) => {
-          if (err) throw err;
-          db.close();
-          resolve();
-        });
-      });
-    } catch (error) {
-      log.error(error);
-      reject(error);
-    }
-  });
-}
 
- async function insert(projectPath,filesDependencies: any) {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const query = new Querys();
-      const db: any = new sqlite3.Database(`${projectPath}/scan_db`, sqlite3.OPEN_READWRITE, (err: any) => {
-      db.serialize(() => {
-        db.run('begin transaction');
-        filesDependencies.forEach((file) => {
-          file.dependenciesList.forEach((dependency) => {
-            db.run(
-              query.SQL_DEPENDENCIES_INSERT,
-              file.fileId,
-              dependency.purl ? decodeURIComponent(dependency.purl) : null,
-              dependency.version ? dependency.version : null,
-              dependency.scope ? dependency.scope : null,
-              dependency.licensesList.length > 0 &&
-              dependency.licensesList[0] !== ''
-                ? dependency.licensesList.join(',')
-                : null,
-              dependency.component,
-              dependency.version ? dependency.version : null,
-              dependency.licensesList.length > 0 &&
-              dependency.licensesList[0] !== ''
-                ? dependency.licensesList.join(',')
-                : null
-            );
-          });
-        });
-        db.run('commit', (err: any) => {
-          db.close();
-          if (err) throw err;
-          resolve(true);
-        });
-      });
-    });
-    } catch (err) {
-      reject(err);
-    }
-  });
-}
