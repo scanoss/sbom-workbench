80f4e97d9f8f6299c052ca376db98ac816c0fa1c
franco.stramana <franco.stramana@gmail.com>
2022-05-26 13:52:46 -0300

    Adds search panel ui

--- src/renderer/features/workbench/components/SearchPanel/SearchPanel.tsx
+++ src/renderer/features/workbench/components/SearchPanel/SearchPanel.tsx
@@ -1,15 +1,49 @@
 import React, { useEffect } from 'react';
-import { Box } from '@material-ui/core';
+import { Box, Button, makeStyles } from '@material-ui/core';
 import SearchBox from '@components/SearchBox/SearchBox';
 import { ipcRenderer } from 'electron';
 import { IpcEvents } from '@api/ipc-events';
 import { DataGrid } from '@material-ui/data-grid';
 import { useHistory } from 'react-router-dom';
+import { mapFiles } from '@shared/utils/scan-util';
+import TreeNode from '../TreeNode/TreeNode';
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    position: 'absolute',
+    top: 12,
+    right: 8,
+    zIndex: 1,
+  },
+  dataGrid: {
+    border: 2,
+    '& .MuiDataGrid-cell': {
+      border: 0,
+      padding: '0 3px',
+    },
+    '& .MuiDataGrid-cell.MuiDataGrid-cellCheckbox': {
+      visibility: 'hidden',
+    },
+    '& .MuiDataGrid-cell.MuiDataGrid-cellCheckbox[data-value=true]': {
+      visibility: 'visible !important',
+    },
+    '& .MuiDataGrid-row:hover': {
+      '& .MuiDataGrid-cell.MuiDataGrid-cellCheckbox': {
+        visibility: 'visible',
+      },
+    },
+    '& .MuiButtonBase-root ': {
+      padding: 0,
+    },
+  },
+}));
 
 const SearchPanel = () => {
   const history = useHistory();
+  const classes = useStyles();
 
   const [results, setResults] = React.useState<any[]>([]);
+  const [selected, setSelected] = React.useState<any[]>([]);
 
   const onSearchHandler = (query: string) => {
     ipcRenderer.send(IpcEvents.SEARCH_ENGINE_SEARCH, { query });
@@ -17,11 +51,7 @@ const SearchPanel = () => {
 
   const onSearchResponse = (event, data) => {
     setResults(
-      data.map(({ id, path }) => ({
-        id,
-        path,
-        filename: path.split('/').pop(),
-      }))
+      mapFiles(data)
     );
   };
 
@@ -30,6 +60,14 @@ const SearchPanel = () => {
       pathname: '/workbench/search/file',
       search: `?path=file|${encodeURIComponent(row.path)}`,
     });
+  };
+
+  const onSelectionHandler = (data, details) => {
+    setSelected(data)
+  };
+
+  const onIdentifyAllHandler = () => {
+    console.log('Identify all');
   };
 
   const setupListeners = () => {
@@ -52,7 +90,7 @@ const SearchPanel = () => {
           <div className="search-panel mt-3">
             <div className="search-panel-input">
               <SearchBox
-                placeholder="Search keywords"
+                placeholder="Search by keywords"
                 onChange={onSearchHandler}
               />
             </div>
@@ -60,25 +98,28 @@ const SearchPanel = () => {
         </Box>
       </header>
       <main className="panel-body">
+        <Button size="small" className={classes.button} onClick={onIdentifyAllHandler}>Identify All</Button>
         <DataGrid
+          className={classes.dataGrid}
           columns={[
             {
               field: 'filename',
-              headerName: 'Results',
+              headerName: `${selected?.length} of ${results?.length} rows selected`,
               editable: false,
               sortable: false,
               flex: 1,
               // eslint-disable-next-line react/display-name
-              renderCell: ({ row }) => <span title={row.path}>{row.filename}</span>,
+              renderCell: ({ row }) => <TreeNode node={row} />,
             },
           ]}
           rows={results}
-          rowHeight={26}
+          rowHeight={23}
           checkboxSelection
           disableColumnMenu
           disableSelectionOnClick
           hideFooterPagination
           onRowClick={onRowClick}
+          onSelectionModelChange={onSelectionHandler}
         />
       </main>
       <footer className="panel-footer">
