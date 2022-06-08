8830916e70f64ed8ad4715396af41b5c7965429b
Agustin Groh <agusgroh@gmail.com>
2022-06-08 08:29:24 -0300

    DT-710 Fixes the bug when creating search index on re-scan

--- src/main/task/search/indexTask/IndexTask.ts
+++ src/main/task/search/indexTask/IndexTask.ts
@@ -11,14 +11,14 @@ export class IndexTask extends EventEmitter implements ITask<Electron.WebContent
     const indexer = new Indexer(event);
     const filesToIndex = this.fileAdapter(files);
     const index = indexer.index(filesToIndex);
-    const projectPath = workspace.getOpenedProjects()[0].getMyPath();
+    const projectPath = workspace.getOpenedProjects()[0].metadata.getMyPath();
     await indexer.saveIndex(index, `${projectPath}/dictionary/`);
   }
 
   private fileAdapter(modelFiles: any): Array<IIndexer> {
     const filesToIndex = [];
     const p = workspace.getOpenedProjects()[0];
-    const scanRoot = p.getScanRoot();
+    const scanRoot = p.metadata.getScanRoot();
     modelFiles.forEach((file: any) => {
       if (file.filter !== 'FILTERED') filesToIndex.push({ fileId: file.id, path: `${scanRoot}${file.path}` });
     });
