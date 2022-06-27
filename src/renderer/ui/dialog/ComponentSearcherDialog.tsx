0e39730af5fd3dda9555fc9db041361f38b02297
Agustin Groh <agusgroh@gmail.com>
2022-06-27 14:03:00 -0300

    DT-727 Adds advanced search in component catalogue searcher

--- src/renderer/ui/dialog/ComponentSearcherDialog.tsx
+++ src/renderer/ui/dialog/ComponentSearcherDialog.tsx
@@ -1,4 +1,13 @@
-import { Dialog, Paper, DialogActions, Button, makeStyles, InputBase, CircularProgress } from '@material-ui/core';
+import {
+  Dialog,
+  Paper,
+  Button,
+  IconButton,
+  makeStyles,
+  InputBase,
+  CircularProgress,
+  TextField,
+} from '@material-ui/core';
 import React, { useContext, useEffect, useState } from 'react';
 import { DialogResponse, DIALOG_ACTIONS } from '@context/types';
 import { componentService } from '@api/services/component.service';
@@ -6,8 +15,15 @@ import { DataGrid } from '@material-ui/data-grid';
 import useApi from '@hooks/useApi';
 import { importGlobalComponent } from '@store/component-store/componentThunks';
 import { useDispatch } from 'react-redux';
-import { DialogContext } from "@context/DialogProvider";
+import { DialogContext } from '@context/DialogProvider';
+import { Autocomplete } from '@material-ui/lab';
+import { AppDispatch } from '@store/store';
+import CompareArrowsIcon from '@material-ui/icons/CompareArrows';
+import SearchIcon from '@material-ui/icons/Search';
+import { packages } from '@assets/data/ComponentCatalogPackages';
+import CloseIcon from '@material-ui/icons/Close';
 import { IComponentResult } from '../../../main/task/componentCatalog/iComponentCatalog/IComponentResult';
+import { ISearchComponent } from '../../../main/task/componentCatalog/iComponentCatalog/ISearchComponent';
 
 const useStyles = makeStyles((theme) => ({
   size: {
@@ -27,6 +43,18 @@ const useStyles = makeStyles((theme) => ({
       outline: 'none !important',
     },
   },
+  title: {
+    fontSize: 22,
+    fontStyle: 'normal',
+    fontWeight: 400,
+    letterSpacing: 0,
+  },
+  titleContainer: {
+    display: 'flex',
+    flexFlow: 'row',
+    borderBottom: '1px solid #D4D4D8',
+    height: 55,
+  },
 }));
 
 interface ComponentSearcherDialogProps {
@@ -38,42 +66,67 @@ interface ComponentSearcherDialogProps {
 
 const ComponentSearcherDialog = (props: ComponentSearcherDialogProps) => {
   const classes = useStyles();
-  const dispatch = useDispatch();
+  const dispatch = useDispatch<AppDispatch>();
   const { open, query, onClose, onCancel } = props;
   const { data, error, loading, execute } = useApi<IComponentResult[]>();
   const dialogCtrl = useContext<any>(DialogContext);
   const [results, setResults] = React.useState<any[]>([]);
   const [selected, setSelected] = React.useState<any[]>([]);
-  const [queryTerm, setQueryTerm] = useState<string>('');
+  const [queryTerm, setQueryTerm] = useState<ISearchComponent>({ search: '', package: '', component: '', vendor: '' });
+  const [advanceSearch, setAdavanceSearch] = useState<boolean>(false);
+  const [searchText, setSearchText] = useState<string>('Advanced search');
+  const [componentSelected, setComponentSelected] = useState<IComponentResult>(null);
 
   const handleSearch = async (e) => {
     e.preventDefault();
-    execute(() => componentService.getGlobalComponents({ search: queryTerm, limit: 100 }));
+    if (advanceSearch) {
+      const { vendor, component } = queryTerm;
+      execute(() =>
+        componentService.getGlobalComponents({ ...{ vendor, component, package: queryTerm.package }, limit: 100 })
+      );
+    } else {
+      const { search } = queryTerm;
+      execute(() => componentService.getGlobalComponents({ ...{ search, packages: queryTerm.package }, limit: 100 }));
+    }
   };
 
-  const onSelectionHandler = (data) => {
-    setSelected(data);
+  const onSelectionHandler = async (data) => {
+    const componentSelected: IComponentResult = results.find((el) => el.id === data[0]);
+    setComponentSelected(componentSelected);
+
   };
 
-  const handleClose = async (e) => {
-    e.preventDefault();
+  const handleClose = async () => {
     try {
-      // Takes the selected component
-      const componentSelected: IComponentResult = results.find((el) => el.id === selected[0]);
-      // TODO: see https://github.com/reduxjs/redux-toolkit/issues/1589
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      const response = await dispatch(importGlobalComponent(componentSelected)).unwrap();
-      onClose({ action: DIALOG_ACTIONS.OK, data: response });
+      const dialogResponse = await dialogCtrl.openConfirmDialog(
+        `<h3>Import Component</h3><p>Do you want to add ${componentSelected.component} to your catalogue?</p>`,
+        { label: 'Add to Catalogue', role: 'accept' },
+        false
+      );
+      if (dialogResponse.action === DIALOG_ACTIONS.OK) {
+        console.log(componentSelected);
+        const response = await dispatch(importGlobalComponent(componentSelected)).unwrap();
+        onClose({ action: DIALOG_ACTIONS.OK, data: response });
+      }
     } catch (error: any) {
       await dialogCtrl.openConfirmDialog(error.message, { label: 'Accept', role: 'accept' }, true);
       console.log('error', error);
     }
   };
 
+  const setAdvancedSearch = () => {
+    setAdavanceSearch(!advanceSearch);
+    if (searchText === 'Standard Search') setSearchText('Advanced Search');
+    else setSearchText('Standard Search');
+  };
+
   useEffect(() => {
     setResults(data ? data.map((value, index) => ({ ...value, id: index })) : []);
   }, [data]);
+
+  useEffect(() => {
+   if(componentSelected) handleClose();
+  }, [componentSelected]);
 
   return (
     <Dialog
@@ -85,28 +138,103 @@ const ComponentSearcherDialog = (props: ComponentSearcherDialogProps) => {
       open={open}
       onClose={onCancel}
     >
-      <span className="dialog-title">Search components online</span>
+      <div className={classes.titleContainer}>
+        <div
+          style={{
+            display: 'flex',
+            width: '70%',
+            justifyContent: 'flex-start',
+            paddingLeft: '3%',
+            alignItems: 'center',
+          }}
+        >
+          <p className={classes.title}>Online Component Search</p>
+        </div>
+        <div style={{ display: 'flex', width: '30%', justifyContent: 'flex-end' }}>
+          <IconButton aria-label="close" onClick={onCancel}>
+            <CloseIcon />
+          </IconButton>
+        </div>
+      </div>
 
       <form onSubmit={handleSearch}>
         <div className="dialog-content">
           <div className="dialog-row">
             <div className="dialog-form-field d-flex space-between">
-              <Paper className="dialog-form-field-control w-100 mr-4">
-                <InputBase
-                  disabled={loading}
-                  name="search"
-                  fullWidth
-                  value={queryTerm}
-                  placeholder="Search"
-                  onChange={(e) => setQueryTerm(e.target.value)}
-                />
-                {loading && <CircularProgress size={18} className="mr-2" />}
-              </Paper>
+              {advanceSearch === false ? (
+                <Paper className="dialog-form-field-control w-100 mr-4">
+                  <InputBase
+                    disabled={loading}
+                    name="search"
+                    fullWidth
+                    value={queryTerm.search}
+                    placeholder="Search"
+                    onChange={(e) => setQueryTerm({ ...queryTerm, search: e.target.value })}
+                  />
+                  {loading && <CircularProgress size={18} className="mr-2" />}
+                </Paper>
+              ) : (
+                <>
+                  <Paper className="dialog-form-field-control w-100 mr-4">
+                    <InputBase
+                      disabled={loading}
+                      name="Name"
+                      fullWidth
+                      value={queryTerm.component}
+                      placeholder="Name"
+                      onChange={(e) => setQueryTerm({ ...queryTerm, component: e.target.value })}
+                    />
+                    {loading && <CircularProgress size={18} className="mr-2" />}
+                  </Paper>
+                  <div>
+                    <Paper>
+                      <InputBase
+                        disabled={loading}
+                        name="Vendor"
+                        fullWidth
+                        value={queryTerm.vendor}
+                        placeholder="Vendor"
+                        onChange={(e) => setQueryTerm({ ...queryTerm, vendor: e.target.value })}
+                      />
+                      {loading && <CircularProgress size={18} className="mr-2" />}
+                    </Paper>
+                  </div>
+                </>
+              )}
 
-              <Button disabled={loading} type="submit" variant="outlined" color="primary">
-                Search
-              </Button>
+              <div>
+                <Autocomplete
+                  id="combo-box-demo"
+                  options={packages}
+                  onChange={(event, value) => {
+                    setQueryTerm({ ...queryTerm, package: value || '' });
+                  }}
+                  getOptionLabel={(option) => option.toString()}
+                  style={{
+                    width: 150,
+                    height: 10,
+                  }}
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      label="Package"
+                      InputProps={{ ...params.InputProps, disableUnderline: true }}
+                    />
+                  )}
+                />
+              </div>
+              <IconButton disabled={loading} type="submit">
+                {' '}
+                <SearchIcon />
+              </IconButton>
             </div>
+          </div>
+          <div className="dialog-row">
+            <IconButton style={{ fontSize: '10px', borderRadius: '0' }} onClick={setAdvancedSearch}>
+              {' '}
+              <CompareArrowsIcon style={{ fontSize: '20px', paddingRight: '2px' }} />
+              {searchText}
+            </IconButton>
           </div>
         </div>
       </form>
@@ -127,12 +255,6 @@ const ComponentSearcherDialog = (props: ComponentSearcherDialogProps) => {
           onSelectionModelChange={onSelectionHandler}
           hideFooter
         />
-        <DialogActions>
-          <Button onClick={onCancel}>Cancel</Button>
-          <Button disabled={selected.length === 0} type="submit" variant="contained" color="secondary">
-            Select
-          </Button>
-        </DialogActions>
       </form>
     </Dialog>
   );
