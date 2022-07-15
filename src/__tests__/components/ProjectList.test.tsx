6d96dc5bf8ad2f7fa45fb56f3503f3c34919d60a
franco.stramana <franco.stramana@gmail.com>
2022-07-15 10:51:06 -0300

    DT-752 Fixes test suite

--- src/__tests__/components/ProjectList.test.tsx
+++ src/__tests__/components/ProjectList.test.tsx
@@ -1,47 +1,24 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import toJson from 'enzyme-to-json';
-
-import { configure, shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { configure } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 import ProjectList from '../../renderer/features/workspace/pages/Components/ProjectList';
 
-configure({ adapter: new Adapter() });
+const props = {
+  projects: [],
+  searchQuery: '',
+  onProjectClick: jest.fn(),
+  onProjectDelete: jest.fn(),
+  onProjectRestore: jest.fn(),
+  onProjectCreate: jest.fn(),
+  onProjectImport: jest.fn(),
+  onProjectExport: jest.fn(),
+  onProjectRescan: jest.fn(),
+};
 
-function setup() {
-  const props = {
-    projects: [],
-    searchQuery: '',
-    onProjectClick: jest.fn(),
-    onProjectDelete: jest.fn(),
-    onProjectRestore: jest.fn(),
-    onProjectCreate: jest.fn(),
-    onProjectImport: jest.fn(),
-    onProjectExport: jest.fn(),
-    onProjectRescan: jest.fn(),
-  };
-
-  const enzymeWrapper = shallow(<ProjectList {...props} />);
-
-  return {
-    props,
-    enzymeWrapper,
-  };
-}
 describe('ProjectList', () => {
   it('should render an empty container', () => {
-    const { enzymeWrapper } = setup();
-    const tree = toJson(enzymeWrapper);
-
-    // expect(tree).toMatchSnapshot();
-    expect(tree.children[0].props.className).toEqual('empty-container');
-  });
-
-  it('should render a projects table', () => {
-    const { enzymeWrapper } = setup();
-    enzymeWrapper.setProps({ projects: [{ name: 'test' }] });
-    const tree = toJson(enzymeWrapper);
-
-    expect(tree.children[0].children[0].props.className).toEqual('projects-table');
+    const component = renderer.create(<ProjectList {...props} />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
