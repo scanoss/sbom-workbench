64245834d90ccf61e0795dcc345afa0ecfe4f7c7
Agustin Groh <agusgroh@gmail.com>
2022-07-19 10:37:04 -0300

    DT-789 Fixes bug when set as original with keep option

--- src/main/batch/Ignore.ts
+++ src/main/batch/Ignore.ts
@@ -5,12 +5,12 @@ import { NodeStatus } from '../workspace/tree/Node';
 import { workspace } from '../workspace/Workspace';
 import { Batch } from './Batch';
 import { Restore } from './Restore';
-import { fileService } from "../services/FileService";
+import { fileService } from '../services/FileService';
 
 export class Ignore extends Batch {
   private queryBuilder: QueryBuilder;
 
-  constructor(params:IBatchInventory) {
+  constructor(params: IBatchInventory) {
     super(params);
     const filter = workspace.getOpenedProjects()[0].getGlobalFilter();
     this.queryBuilder = QueryBuilderCreator.create({
