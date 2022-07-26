52f603cdc9814cdac071ab0663383706e92dbad4
franco.stramana <franco.stramana@gmail.com>
2022-07-26 20:24:39 -0300

    Add vulnerabilities table

--- src/renderer/features/workbench/pages/report/pages/vulnerabilities/VulnerabilitiesReport.tsx
+++ src/renderer/features/workbench/pages/report/pages/vulnerabilities/VulnerabilitiesReport.tsx
@@ -1,31 +1,39 @@
 import React, { useState } from 'react';
-import {IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
 
+import { IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 import ReceiptLongOutlinedIcon from '@mui/icons-material/ReceiptLongOutlined';
 import ContentCopyOutlinedIcon from '@mui/icons-material/ContentCopyOutlined';
 
+import TableCellActions from '@components/TableCellActions/TableCellActions';
+
 const VulnerabilitiesReport = () => {
-  const [data, setData] = useState<any[]>([
-    {
-      component: 'Angular',
-      severity: 'medium',
-      cve: 'CVE-2014-8180',
-      source: 'Nvd',
-      introduced: '3.2.7',
-      reported: '3.2.7',
-      patched: '3.2.8',
-    },
-  ]);
+  const [data, setData] = useState<any[]>(
+    Array(20)
+      .fill(null)
+      .map((i, index) => ({
+        component: 'Angular',
+        severity: 'medium',
+        cve: `CVE-2014-8181${index}`,
+        source: 'Nvd',
+        introduced: '3.2.7',
+        reported: '3.2.7',
+        patched: '3.2.8',
+      }))
+  );
+
+  const onSeeDescriptionClickHandler = (_e, item) => {};
+  const onCopyCVEClickHandler = (_e, item) => {};
+
   return (
     <>
       <section id="VulnerabilitiesReportPage" className="app-page">
         <header className="app-header">
           <h1 className="header-title">Detected Vulnerabilities</h1>
-          <section className="subheader">filters</section>
+          <section className="subheader">Filter</section>
         </header>
         <main className="app-content">
           <TableContainer component={Paper}>
-            <Table className="projects-table" aria-label="projects table">
+            <Table stickyHeader className="selectable" aria-label="vulnerabilities table">
               <TableHead>
                 <TableRow>
                   <TableCell>Component</TableCell>
@@ -39,23 +47,33 @@ const VulnerabilitiesReport = () => {
                 </TableRow>
               </TableHead>
               <TableBody>
-                {data?.map((vulnerability) => (
-                  <TableRow key={vulnerability.cve}>
-                    <TableCell>{vulnerability.component}</TableCell>
-                    <TableCell>{vulnerability.severity}</TableCell>
-                    <TableCell>{vulnerability.cve}</TableCell>
-                    <TableCell>{vulnerability.source}</TableCell>
-                    <TableCell>{vulnerability.introduced}</TableCell>
-                    <TableCell>{vulnerability.reported}</TableCell>
-                    <TableCell>{vulnerability.patched}</TableCell>
-                    <TableCell className="table-actions">
-                      <IconButton aria-label="description" size="small">
+                {data?.map((item) => (
+                  <TableRow key={item.cve}>
+                    <TableCell>{item.component}</TableCell>
+                    <TableCell>{item.severity}</TableCell>
+                    <TableCell>{item.cve}</TableCell>
+                    <TableCell>{item.source}</TableCell>
+                    <TableCell>{item.introduced}</TableCell>
+                    <TableCell>{item.reported}</TableCell>
+                    <TableCell>{item.patched}</TableCell>
+                    <TableCellActions>
+                      <IconButton
+                        title="See description"
+                        aria-label="see description"
+                        size="small"
+                        onClick={(e) => onSeeDescriptionClickHandler(e, item)}
+                      >
                         <ReceiptLongOutlinedIcon fontSize="inherit" />
                       </IconButton>
-                      <IconButton aria-label="description" size="small">
+                      <IconButton
+                        title="Copy CVE"
+                        aria-label="copy cve"
+                        size="small"
+                        onClick={(e) => onCopyCVEClickHandler(e, item)}
+                      >
                         <ContentCopyOutlinedIcon fontSize="inherit" />
                       </IconButton>
-                    </TableCell>
+                    </TableCellActions>
                   </TableRow>
                 ))}
               </TableBody>
