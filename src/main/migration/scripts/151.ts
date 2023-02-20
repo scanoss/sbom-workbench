926369d10e44b1f7585e20e130c14b730bcbf82f
Agustin Isasmendi <isasmendi.agus@gmail.com>
2023-02-20 17:56:49 +0100

    Updates version
    Updates scanoss to 0.8.1 with integration fixes

--- src/main/migration/scripts/151.ts
+++ src/main/migration/scripts/151.ts
@@ -2,5 +2,6 @@ import { userSettingService } from "../../services/UserSettingService";
 
 export async function wsMigration151(): Promise<void> {
   userSettingService.setSetting('PAC', '');
+  userSettingService.setSetting('VERSION', '1.5.1');
   await userSettingService.save();
 }
