5660c1b7a5e20e9c0aeda8957e60b7ed6a410c9d
Agustin Isasmendi <isasmendi.agus@gmail.com>
2023-03-01 15:42:15 +0100

    Update version

--- src/main/migration/scripts/151.ts
+++ src/main/migration/scripts/151.ts
@@ -1,7 +1,7 @@
-import { userSettingService } from "../../services/UserSettingService";
+import { userSettingService } from '../../services/UserSettingService';
 
 export async function wsMigration151(): Promise<void> {
   userSettingService.setSetting('PAC', '');
-  userSettingService.setSetting('VERSION', '1.5.1');
+  userSettingService.setSetting('VERSION', '1.5.2');
   await userSettingService.save();
 }
