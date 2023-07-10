e5dd3569feb4c289aa72efae406c3258acb1ab77
Agustin Groh <agusgroh@gmail.com>
2023-07-10 12:04:49 -0300

    DT-1014 Creates migration for search index  v1.8.0

--- src/main/modules/searchEngine/indexer/IndexerAdapter.ts
+++
@@ -1,7 +0,0 @@
-export class IndexerAdapter {
-    public getFileIndex(filePath: string): string {
-        const map : Record<string,string> = { 'cfg.json': 'cfg', 'ctx.json':'ctx', 'map.json':'map','reg.json':'reg' };
-        return map[filePath];
-    }
-}
-
