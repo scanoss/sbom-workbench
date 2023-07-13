7c28a8dd031e015611f36a2ccf0d87d455697bdd
Agustin Groh <agusgroh@gmail.com>
2023-07-13 09:48:57 -0300

    DT-1020 Adds thread for decompress task

--- src/main/threads/scanner.ts
+++ src/main/threads/scanner.ts
@@ -1,21 +1,18 @@
-import { DecompressionManager } from 'scanoss';
-
-function run(path: string) {
-  const decompressionManager = new DecompressionManager();
-  console.log("hey Agus, estoy dentro de un thread :)");
-  console.log(path);
-  console.log(decompressionManager);
-
-  process.parentPort.postMessage({
-    event: 'event-test',
-  });
-}
-
 /**
  * Entry point of scanner subprocess
  */
-process.parentPort.once('message', (e) => {
-  // const [port] = e.ports;
+import { DecompressThread } from './scanner/DecompressThread';
 
-  run('test');
-})
+
+process.parentPort.once('message', async (e) => {
+  // const [port] = e.ports;
+  const { action, data } = e.data;
+  if (action === 'DECOMPRESS') {
+    const decompressThread = new DecompressThread(data);
+    const success = await decompressThread.run();
+    process.parentPort.postMessage({
+      event: success ? 'success' : 'error',
+      data: success
+    });
+  }
+});
