84b388949b9a608dda3f6f1013bb4628cce3bd97
Agustin Isasmendi <isasmendi.agus@gmail.com>
2023-10-03 15:39:54 +0200

    DT-1048 Create migration to split BASE URL and endpoints

--- src/main/migration/WorkspaceMigration.ts
+++ src/main/migration/WorkspaceMigration.ts
@@ -1,6 +1,7 @@
 import { Migration } from './Migration';
 import { wsMigration140 } from './scripts/140';
-import { wsMigration151 } from "./scripts/151";
+import { wsMigration151 } from './scripts/151';
+import { wsMigration183 } from './scripts/183';
 
 export class WorkspaceMigration extends Migration {
   private scripts: Record<string, Array<(data: string) => void>>;
@@ -14,6 +15,7 @@ export class WorkspaceMigration extends Migration {
       '0.11.1': [], // Oldest compatible version
       '1.4.0': [wsMigration140],
       '1.5.1': [wsMigration151],
+      '1.8.3': [wsMigration183],
     }
   }
 
