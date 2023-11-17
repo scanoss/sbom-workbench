f689b38acd029845fb2cd3a21d53927dad572a7f
agusgroh <agusgroh@gmail.com>
2023-11-17 09:33:07 -0300

    DT-1082 Adds Cryptography scanner type depending on api key

--- src/main/services/ProjectService.ts
+++ src/main/services/ProjectService.ts
@@ -1,27 +1,43 @@
 import log from 'electron-log';
 import {
   ExtractFromProjectDTO,
-  INewProject, Inventory,
+  INewProject,
+  Inventory,
   InventoryKnowledgeExtraction,
   ProjectState,
-  ReuseIdentificationTaskDTO
+  ReuseIdentificationTaskDTO,
 } from '../../api/types';
-import {Project} from '../workspace/Project';
-import {workspace} from '../workspace/Workspace';
-import {modelProvider} from './ModelProvider';
-import {Scanner} from '../task/scanner/types';
-import {userSettingService} from './UserSettingService';
-import {ProjectFilterPath} from '../workspace/filters/ProjectFilterPath';
-import {CodeScannerPipelineTask} from '../task/scanner/scannerPipeline/CodeScannerPipelineTask';
-import {ScannerPipelineFactory} from "../task/scanner/scannerPipelineFactory/ScannerPipelineFactory";
-import ScannerType = Scanner.ScannerType;
-import ScannerMode = Scanner.ScannerMode;
-import {ProjectKnowledgeExtractor} from "../modules/projectKnowledge/ProjectKnowledgeExtractor";
+import { Project } from '../workspace/Project';
+import { workspace } from '../workspace/Workspace';
+import { modelProvider } from './ModelProvider';
+import { Scanner } from '../task/scanner/types';
+import { userSettingService } from './UserSettingService';
+import { ProjectFilterPath } from '../workspace/filters/ProjectFilterPath';
+import { CodeScannerPipelineTask } from '../task/scanner/scannerPipeline/CodeScannerPipelineTask';
+import { ScannerPipelineFactory } from '../task/scanner/scannerPipelineFactory/ScannerPipelineFactory';
+import { ProjectKnowledgeExtractor } from '../modules/projectKnowledge/ProjectKnowledgeExtractor';
 import { ReuseIdentificationTask } from '../task/reuseIdentification/ReuseIdentificationTask';
+import ScannerMode = Scanner.ScannerMode;
+import ScannerType = Scanner.ScannerType;
 
 class ProjectService {
+  private setCryptographyScanType(p: Project): void {
+    if (p.getGlobalApiKey()) {
+      const types = p.metadata.getScannerConfig().type;
+      types.push(ScannerType.CRYPTOGRAPHY);
+      const uniqueTypes = new Set(types);
+      p.metadata.getScannerConfig().type = Array.from(uniqueTypes);
+    } else {
+      p.metadata.getScannerConfig().type.filter((t) => t !== ScannerType.CRYPTOGRAPHY);
+    }
+  }
+
   public async createProject(projectDTO: INewProject) {
     const p = await this.create(projectDTO);
+    // Add crypto scanner config depending on API Key token
+    this.setCryptographyScanType(p);
+    p.save();
+
     await ScannerPipelineFactory.getScannerPipeline(projectDTO.scannerConfig.source).run(p);
   }
 
@@ -29,6 +45,11 @@ class ProjectService {
     await workspace.closeAllProjects();
     const p = await workspace.getProject(new ProjectFilterPath(projectPath));
     p.metadata.getScannerConfig().mode = Scanner.ScannerMode.RESCAN;
+
+    // Add crypto scanner config depending on API Key token
+    this.setCryptographyScanType(p);
+    p.save();
+
     await ScannerPipelineFactory.getScannerPipeline(p.metadata.getScannerConfig().source).run(p);
   }
 
@@ -43,8 +64,7 @@ class ProjectService {
   private async createNewProject(projectDTO: INewProject): Promise<Project> {
     const p = await workspace.createProject(projectDTO);
     await modelProvider.init(p.getMyPath());
-    log.transports.file.resolvePath = () =>
-      `${p.metadata.getMyPath()}/project.log`;
+    log.transports.file.resolvePath = () => `${p.metadata.getMyPath()}/project.log`;
     p.state = ProjectState.OPENED;
     p.save();
     return p;
@@ -52,7 +72,7 @@ class ProjectService {
 
   private async create(
     projectDTO: INewProject,
-    event: Electron.WebContents = null
+    event: Electron.WebContents = null,
   ): Promise<Project> {
     await workspace.closeAllProjects();
     projectDTO.scannerConfig.mode = ScannerMode.SCAN;
@@ -74,18 +94,16 @@ class ProjectService {
     }
   }
 
-  public async extractProjectKnowledgeInventoryData(param: ExtractFromProjectDTO): Promise<InventoryKnowledgeExtraction>{
+  public async extractProjectKnowledgeInventoryData(param: ExtractFromProjectDTO): Promise<InventoryKnowledgeExtraction> {
     const projectKnowledgeExtractor = new ProjectKnowledgeExtractor(param);
     const inventoryKnowledgeData = await projectKnowledgeExtractor.extractInventoryData();
     return inventoryKnowledgeData;
   }
 
-  public async acceptInventoryKnowledge(param: ReuseIdentificationTaskDTO): Promise<Array<Inventory>>{
-   const inventories =  await new ReuseIdentificationTask(param).run();
-  return inventories ;
+  public async acceptInventoryKnowledge(param: ReuseIdentificationTaskDTO): Promise<Array<Inventory>> {
+    const inventories = await new ReuseIdentificationTask(param).run();
+    return inventories;
   }
-
-
 }
 
 export const projectService = new ProjectService();
