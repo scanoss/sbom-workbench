f56f337af47b2e463c8bba2afbc3a853fe0bcb0a
Franco Stramana <franco.stramana@gmail.com>
2023-11-22 13:56:34 -0300

    DT-1098 Ignores vendor folders on dependency scanner

--- src/main/workspace/tree/blackList/BlackListDependencies.ts
+++ src/main/workspace/tree/blackList/BlackListDependencies.ts
@@ -1,10 +1,28 @@
+import * as fs from 'fs';
+import { NameFilter, AbstractFilter } from '../../filtering';
 import { BlackListAbstract } from './BlackListAbstract';
 import Node, { NodeStatus } from '../Node';
 
 export class BlackListDependencies extends BlackListAbstract {
-  private filter: NodeStatus = NodeStatus.FILTERED;
+  private filters: Array<AbstractFilter> = [];
+
+  public constructor(path: string) {
+    super();
+    this.load(path);
+  }
+
+  private load(path: string) {
+    const file = fs.readFileSync(path, 'utf8');
+    const f = JSON.parse(file);
+    const { filters } = f;
+
+    let i: number;
+    for (i = 0; i < filters.length; i += 1) {
+      if (filters[i].ftype === 'NAME' && filters[i].scope === 'FOLDER') this.filters.push(new NameFilter(filters[i].condition, filters[i].value, filters[i].scope));
+    }
+  }
 
   public evaluate(node: Node): boolean {
-    return node.getStatus() === this.filter;
+    return this.filters.some((filter) => !filter.evaluate(node.getPath()));
   }
 }
