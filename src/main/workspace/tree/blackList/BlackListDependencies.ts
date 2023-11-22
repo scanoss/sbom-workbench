efd088dce9c37ee769ae2ceee9e6816afb7bfe68
agusgroh <agusgroh@gmail.com>
2023-11-22 11:14:03 -0300

    DT-1096 avoids scan dependencies in node_modules folder

--- src/main/workspace/tree/blackList/BlackListDependencies.ts
+++ src/main/workspace/tree/blackList/BlackListDependencies.ts
@@ -2,7 +2,7 @@ import { BlackListAbstract } from './BlackListAbstract';
 import Node, { NodeStatus } from '../Node';
 
 export class BlackListDependencies extends BlackListAbstract {
-  private filter: NodeStatus.FILTERED;
+  private filter: NodeStatus = NodeStatus.FILTERED;
 
   public evaluate(node: Node): boolean {
     return node.getStatus() === this.filter;
