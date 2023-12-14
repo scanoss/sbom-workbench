f269e0617ee8570e90c210fd4f80ef9276a9401d
agusgroh <agusgroh@gmail.com>
2023-12-14 09:16:36 -0300

    DT-1114 Add version into vulnerabilities report Co-authored-by: FrancoStramana <franco.stramana@gmail.com>

--- src/renderer/features/workbench/pages/report/pages/vulnerabilities/VulnerabilitiesReport.tsx
+++ src/renderer/features/workbench/pages/report/pages/vulnerabilities/VulnerabilitiesReport.tsx
@@ -54,18 +54,15 @@ const VulnerabilitiesReport = () => {
     const response = await vulnerabilityService.getAll({ type: source });
     data.current = response;
 
-    console.log(response)
     setItems(data.current);
     setComponent(Array.from(new Set(data.current.map((item) => item.componentVersion.name))));
   };
 
-  const filterItems = (items: ComponentVulnerability[]) => {
-    return filter
-      ? items
-          .filter((item) => !filter.component || item.componentVersion.name === filter.component)
-          .filter((item) => !filter.severity || filter.severity.length === 0 || filter.severity.includes(item.vulnerability.severity?.toLowerCase()))
-      : items;
-  };
+  const filterItems = (items: ComponentVulnerability[]) => (filter
+    ? items
+      .filter((item) => !filter.component || item.componentVersion.name === filter.component)
+      .filter((item) => !filter.severity || filter.severity.length === 0 || filter.severity.includes(item.vulnerability.severity?.toLowerCase()))
+    : items);
 
   const onSeeDescriptionClickHandler = (e, item: ComponentVulnerability) => {
     setPopoverContent(item.vulnerability.summary);
@@ -134,20 +131,18 @@ const VulnerabilitiesReport = () => {
                       renderOption={(props, option, { selected }) => (
                         <li {...props}>
                           <Checkbox style={{ marginRight: 8 }} checked={selected} />
-                          <span className={`tag tag-${option} option` }> {option} </span>
+                          <span className={`tag tag-${option} option`}> {option} </span>
                         </li>
                       )}
-                      renderTags={(value: readonly string[], getTagProps) =>
-                        value.map((option: string, index: number) => (
-                          <Chip
-                            key={option}
-                            label={option}
-                            size="small"
-                            {...getTagProps({ index })}
-                            className={`tag tag-${option} mr-1`}
-                          />
-                        ))
-                      }
+                      renderTags={(value: readonly string[], getTagProps) => value.map((option: string, index: number) => (
+                        <Chip
+                          key={option}
+                          label={option}
+                          size="small"
+                          {...getTagProps({ index })}
+                          className={`tag tag-${option} mr-1`}
+                        />
+                      ))}
                       renderInput={(params) => (
                         <TextField
                           {...params}
@@ -189,33 +184,36 @@ const VulnerabilitiesReport = () => {
                     <TableCell className="pb-0 pt-0">
                       <ListItemText
                         primary={item.componentVersion.name}
-                        secondary={item.componentVersion.purl}
+                        secondary={`${item.componentVersion.purl}@${item.componentVersion.version}`}
                       />
                     </TableCell>
                     <TableCell>
                       <span className={`tag tag-${item.vulnerability.severity?.toLowerCase()}`}>{item.vulnerability.severity}</span>
                     </TableCell>
                     <TableCell><Link
-                                  className="d-flex align-center"
-                                  href={`https://nvd.nist.gov/vuln/detail/${item.vulnerability.cve}`}
-                                  target="_blank" rel="noreferrer">
-                                    {item.vulnerability.cve}  <OpenInNewOutlinedIcon className="external-link" fontSize="small" />
-                                  </Link>
+                      className="d-flex align-center"
+                      href={`https://nvd.nist.gov/vuln/detail/${item.vulnerability.cve}`}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      {item.vulnerability.cve}  <OpenInNewOutlinedIcon className="external-link" fontSize="small" />
+                               </Link>
                     </TableCell>
                     <TableCell>{item.vulnerability.source}</TableCell>
                     <TableCell>{item.vulnerability.published}</TableCell>
                     <TableCell>{item.vulnerability.modified}</TableCell>
                     <TableCellActions>
-                      {item.vulnerability.summary &&
+                      {item.vulnerability.summary
+                        && (
                         <IconButton
                           title={t('Tooltip:SeeDescription')}
                           aria-label="see description"
                           size="small"
                           onClick={(e) => onSeeDescriptionClickHandler(e, item)}
                         >
-                          <ReceiptLongOutlinedIcon fontSize="inherit"/>
+                          <ReceiptLongOutlinedIcon fontSize="inherit" />
                         </IconButton>
-                      }
+                        )}
                     </TableCellActions>
                   </TableRow>
                 ))}
