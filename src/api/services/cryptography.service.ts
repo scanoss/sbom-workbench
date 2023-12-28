63b13fd3a3e43d7c72626635788af2c8d8ae6d43
agusgroh <agusgroh@gmail.com>
2023-12-28 15:17:43 -0300

    DT-1136 Adds services to update vulnerabilities and cryptography

---
+++ src/api/services/cryptography.service.ts
@@ -0,0 +1,15 @@
+import { IpcChannels } from '@api/ipc-channels';
+import { BaseService } from './base.service';
+
+class CryptographyService extends BaseService {
+  public async update(): Promise<void> {
+    const response = await window.electron.ipcRenderer.invoke(IpcChannels.CRYPTOGRAPHY_UPDATE);
+    return this.response(response);
+  }
+}
+
+
+
+export const cryptographyService = new CryptographyService();
+
+document.crypto = cryptographyService;
