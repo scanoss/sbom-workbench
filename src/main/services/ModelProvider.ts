a0bdc7d38e6834b85c6cf7cdd9e07d37a1bb648a
Agustin Isasmendi <isasmendi.agus@gmail.com>
2024-02-05 18:40:40 +0100

    DT-1214 Define method to check project access mode Co-authored-by: Agustin Groh <agusgroh@gmail.com>

--- src/main/services/ModelProvider.ts
+++ src/main/services/ModelProvider.ts
@@ -1,8 +1,14 @@
 import { Model } from '../model/Model';
 import { ScanModel } from '../model/ScanModel';
+import sqlite3 from 'sqlite3';
+import path from 'path';
+import { Querys } from '../model/querys_db';
+import { log } from 'console';
 
 class ModelProvider {
   private _model: ScanModel;
+
+  private _workspace: sqlite3.Database | null;
 
   private projectPath: string;
 
@@ -17,11 +23,39 @@ class ModelProvider {
     this._model = value;
   }
 
+  public get getWorkspaceDb(): sqlite3.Database | null {
+    return this._workspace;
+  }
+
+  public async initWorkspace(wsPath: string) {
+    this._workspace = await this.initWorkspaceDb(wsPath);
+  }
+
   public async init(projectPath: string) {
+    // Create scan_db
     await new Model(projectPath).init();
+    // Init all models
     const model = new ScanModel(projectPath);
     this.model = model;
   }
+
+  private async initWorkspaceDb(wsPath: string) {
+    const query = new Querys();
+    return new Promise<sqlite3.Database| null>((resolve, reject) => {
+      const db = new sqlite3.Database(path.join(wsPath,'workspace.sqlite'), (err: any) => {
+        if (err) {
+          console.error("Unable to create Workspace DB");
+          reject(null);
+        } else {
+          db.exec(query.WORKSPACE_LOCK,async () => {
+            resolve(db);
+          });
+        }
+      });
+    });
+  }
+
+
 }
 
 export const modelProvider = new ModelProvider();
