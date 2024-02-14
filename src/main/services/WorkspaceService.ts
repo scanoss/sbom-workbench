f946d966bc0e8b502903df61bea7aae3dc25a7ea
agusgroh <agusgroh@gmail.com>
2024-02-14 14:49:20 -0300

    DT-1227 Fixes bug: App not restart after changing workspace

--- src/main/services/WorkspaceService.ts
+++ src/main/services/WorkspaceService.ts
@@ -1,11 +1,11 @@
-import { app } from 'electron';
+import { BrowserWindow, RelaunchOptions, app, dialog } from 'electron';
+import i18next from 'i18next';
 import { userSettingService } from './UserSettingService';
 
 class WorkspaceService {
-
   public async setCurrent(wsPath: string): Promise<void> {
     const workspaces = userSettingService.get().WORKSPACES;
-    let workspaceIndex =  workspaces.findIndex((w)=> w.PATH === wsPath);
+    const workspaceIndex = workspaces.findIndex((w) => w.PATH === wsPath);
 
     // If workspace path not exists , set default workspace
     if (workspaceIndex < 0) throw new Error('Workspace not found');
@@ -13,11 +13,27 @@ class WorkspaceService {
     userSettingService.set({ DEFAULT_WORKSPACE_INDEX: workspaceIndex });
     await userSettingService.save();
 
-    // Relaunch app
-    app.relaunch();
-    app.exit();
-  }
+    const { response } = await dialog.showMessageBox(
+      BrowserWindow.getFocusedWindow(),
+      {
+        buttons: [i18next.t('Button:RestartLater'), i18next.t('Button:RestartNow')],
+        message: i18next.t('Dialog:YouNeedRestartQuestionWorkspace'),
+      },
+    );
 
+    if (response === 1) {
+      const options: RelaunchOptions = {
+        args: process.argv.slice(1).concat(['--relaunch']),
+        execPath: process.execPath,
+      };
+      if (process.env.APPIMAGE) {
+        options.execPath = process.env.APPIMAGE;
+        options.args.unshift('--appimage-extract-and-run');
+      }
+      app.relaunch(options);
+      app.exit(0);
+    }
+  }
 }
 
-export  const workspaceService = new WorkspaceService();
+export const workspaceService = new WorkspaceService();
