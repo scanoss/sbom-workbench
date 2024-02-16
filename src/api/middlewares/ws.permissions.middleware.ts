c64445b246ec2ae11cb1c38cbbe26452557e47c1
Agustin Isasmendi <isasmendi.agus@gmail.com>
2024-02-16 13:38:04 +0100

    DT-1232 Fixes duplicated dialog on WS permissions error

--- src/api/middlewares/ws.permissions.middleware.ts
+++ src/api/middlewares/ws.permissions.middleware.ts
@@ -14,33 +14,35 @@ export async function wsPermissionsMiddleware() {
   }
 }
 
+let nDialog = 0;
 async function restartApp() {
-  const { username } = os.userInfo();
-  const { response } = await dialog.showMessageBox(
-    BrowserWindow.getFocusedWindow(),
-    {
+  if (nDialog <= 0) {
+    nDialog++;
+
+    const { username } = os.userInfo();
+    const { response } = await dialog.showMessageBox(BrowserWindow.getFocusedWindow(), {
       buttons: ['Accept'],
       message: `User '${username}' does not have access permissions on workspace ${workspace.getMyPath()}\n\nThe app will restart and load the default workspace.`,
-    },
-  );
+    });
 
-  if (response === 0) {
-    await modelProvider.workspace.destroy();
-    await modelProvider.model.destroy();
-    // Set default workspace on accept
-    userSettingService.set({ DEFAULT_WORKSPACE_INDEX: 0 });
-    await userSettingService.save();
+    if (response === 0) {
+      await modelProvider.workspace.destroy();
+      await modelProvider.model.destroy();
+      // Set default workspace on accept
+      userSettingService.set({ DEFAULT_WORKSPACE_INDEX: 0 });
+      await userSettingService.save();
 
-    // Restart app
-    const options: RelaunchOptions = {
-      args: process.argv.slice(1).concat(['--relaunch']),
-      execPath: process.execPath,
-    };
-    if (process.env.APPIMAGE) {
-      options.execPath = process.env.APPIMAGE;
-      options.args.unshift('--appimage-extract-and-run');
+      // Restart app
+      const options: RelaunchOptions = {
+        args: process.argv.slice(1).concat(['--relaunch']),
+        execPath: process.execPath,
+      };
+      if (process.env.APPIMAGE) {
+        options.execPath = process.env.APPIMAGE;
+        options.args.unshift('--appimage-extract-and-run');
+      }
+      app.relaunch(options);
+      app.exit(0);
     }
-    app.relaunch(options);
-    app.exit(0);
   }
 }
