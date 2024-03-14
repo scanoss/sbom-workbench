aa422128cb6b3924b17eb7f022f14f730c3045b7
agusgroh <agusgroh@gmail.com>
2024-03-14 10:49:00 -0300

    DT-1277 Fixes bug on filtered files

--- src/main/task/scanner/scan/CodeScanTask.ts
+++ src/main/task/scanner/scan/CodeScanTask.ts
@@ -1,10 +1,9 @@
-import { ScanTask } from "./ScanTask";
-import { CodeDispatcher } from "../dispatcher/CodeDispatcher";
-import { CodeScannerInputAdapter } from "../adapter/CodeScannerInputAdapter";
-import {Project} from "../../../workspace/Project";
+import { ScanTask } from './ScanTask';
+import { CodeDispatcher } from '../dispatcher/CodeDispatcher';
+import { CodeScannerInputAdapter } from '../adapter/CodeScannerInputAdapter';
+import { Project } from '../../../workspace/Project';
 
 export class CodeScanTask extends ScanTask<CodeDispatcher, CodeScannerInputAdapter> {
-
   constructor(project: Project) {
     super(project, new CodeDispatcher(), new CodeScannerInputAdapter());
   }
