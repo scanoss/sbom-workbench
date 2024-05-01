6b0d980a6d24c2f520d97240423565b0b1d75de2
agusgroh <agusgroh@gmail.com>
2024-05-01 14:17:08 -0300

    SP-621 Adds model for retrieving local cryptography

--- src/main/model/project/models/LocalCryptographyModel.ts
+++ src/main/model/project/models/LocalCryptographyModel.ts
@@ -1,5 +1,16 @@
+import util from 'util';
 import sqlite3 from 'sqlite3';
 import { Model } from '../../Model';
+import { queries } from '../../querys_db';
+import { LocalCryptography } from '../../entity/LocalCryptography';
+
+export interface ILocalCryptographyModel {
+  id: number;
+  file_id: number;
+  path: string;
+  algorithms: string;
+  type: string;
+}
 
 export class LocalCryptographyModel extends Model {
   private connection: sqlite3.Database;
@@ -28,5 +39,17 @@ export class LocalCryptographyModel extends Model {
         });
       });
     });
+  }
+
+  public async findAll() : Promise<Array<LocalCryptography>> {
+    const query = queries.SQL_GET_ALL_LOCAL_CRYPTOGRAPHY;
+    const call = await util.promisify(this.connection.all.bind(this.connection)) as any;
+    const response: Array<ILocalCryptographyModel> = await call(query);
+    const crypto = this.cryptographyAdapter(response);
+    return crypto;
+  }
+
+  private cryptographyAdapter(crypto: Array <ILocalCryptographyModel>): Array<LocalCryptography> {
+    return crypto.map((c) => { return { file: c.path, algorithms: JSON.parse(c.algorithms) }; });
   }
 }
