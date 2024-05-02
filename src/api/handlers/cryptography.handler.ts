92e84ec131043cce8c2234102c50328530be8d03
agusgroh <agusgroh@gmail.com>
2024-05-02 15:20:37 -0300

    SP-622 Adds getAll crypto service

--- src/api/handlers/cryptography.handler.ts
+++ src/api/handlers/cryptography.handler.ts
@@ -3,6 +3,7 @@ import api from '../api';
 import { cryptographyService } from '../../main/services/CryptographyService';
 import { IpcChannels } from '../ipc-channels';
 import { Response } from '../Response';
+import { CryptographyGetAllDTO } from '../dto';
 
 api.handle(IpcChannels.CRYPTOGRAPHY_UPDATE, async (event) => {
   try {
@@ -10,6 +11,16 @@ api.handle(IpcChannels.CRYPTOGRAPHY_UPDATE, async (event) => {
     return Response.ok({ message: 'Cryptography updated successfully', data });
   } catch (error: any) {
     log.error('[Cryptography Update]: ', error);
+    return Response.fail({ message: error.message });
+  }
+});
+
+api.handle(IpcChannels.CRYPTOGRAPHY_GET_ALL, async (event, { type }: CryptographyGetAllDTO) => {
+  try {
+    const data = await cryptographyService.getAll(type);
+    return Response.ok({ message: 'Cryptography retrieved successfully', data });
+  } catch (error: any) {
+    log.error('[Cryptography get all]: ', error);
     return Response.fail({ message: error.message });
   }
 });
