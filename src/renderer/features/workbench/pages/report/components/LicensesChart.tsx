dbbe3aa30d8e5d3b7090ae3baf21c2e3ab5564ef
Franco Stramana <franco.stramana@gmail.com>
2024-05-06 13:54:59 -0300

    SP-641 Adds crypto report section

--- src/renderer/features/workbench/pages/report/components/LicensesChart.tsx
+++ src/renderer/features/workbench/pages/report/components/LicensesChart.tsx
@@ -57,7 +57,7 @@ const LicensesChart = ({ data }) => {
             data: data.map((d) => d.value),
             borderWidth: 0,
             backgroundColor: colorsForLicense,
-            cutout: 65,
+            cutout: 55,
           },
         ],
       },
