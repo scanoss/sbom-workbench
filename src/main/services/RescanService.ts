d854eadc6fbead6449ba7c5bf412db8e624b3b91
agusgroh <agusgroh@gmail.com>
2024-06-13 16:50:37 -0300

    DT-1297 fix: Rescan does not update dependencies on database

--- src/main/services/RescanService.ts
+++ src/main/services/RescanService.ts
@@ -1,16 +1,12 @@
 import { IDependencyResponse } from 'scanoss';
-import fs from 'fs';
 import { utilHelper } from '../helpers/UtilHelper';
 import { NodeStatus } from '../workspace/tree/Node';
 import { componentService } from './ComponentService';
-import { dependencyService } from './DependencyService';
 import { modelProvider } from './ModelProvider';
 import { IInsertResult } from '../model/interfaces/IInsertResult';
-import { treeService } from './TreeService';
-import { Tree } from '../workspace/tree/Tree';
 
 class RescanService {
-  private async deleteUnusedComponents(): Promise<void> {
+  public async deleteUnusedComponents(): Promise<void> {
     const notValidDetectedComponents: number[] = await modelProvider.model.component.getNotValid();
     if (notValidDetectedComponents.length > 0) {
       await modelProvider.model.component.deleteByID(
@@ -81,56 +77,15 @@ class RescanService {
       await modelProvider.model.file.deleteDirty();
       await modelProvider.model.component.updateOrphanToManual();
       await componentService.importComponents();
-
-      // DEPENDENCIES
-      const dependencies = JSON.parse(
-        await fs.promises.readFile(`${projectPath}/dependencies.json`, 'utf-8'),
-      );
-
-      // Insert new dependencies
-      await dependencyService.insert(dependencies);
-
-
-      // delete dirty dependencies
-      await modelProvider.model.dependency.deleteDirty(
-        this.dirtyModelDependencyAdapter(dependencies),
-      );
-
-      // Delete dirty dependencies inventories
-      await modelProvider.model.inventory.deleteDirtyDependencyInventories();
-
-      // Delete unused components
-      await this.deleteUnusedComponents();
-
       // Updates most reliable license for each component
       const mostReliableLicensePerComponent = await modelProvider.model.component.getMostReliableLicensePerComponent();
       await modelProvider.model.component.updateMostReliableLicense(
         mostReliableLicensePerComponent,
       );
-
-      // Update the dependencies status on file tree
-      await this.reScanUpdateDependenciesOnFileTree(projectPath);
     } catch (err: any) {
-      console.error('[ RESCAN DB ]', err);
-      throw new Error('[ RESCAN DB ] Unable to insert new results');
+      console.error('[ Rescan Service ]', err);
+      throw err;
     }
-  }
-
-  public async reScanUpdateDependenciesOnFileTree(projectPath: string) {
-    // update dependency status on file tree
-    const m = await fs.promises.readFile(`${projectPath}/metadata.json`, 'utf8');
-    const metadata = JSON.parse(m);
-    const fileTree = await fs.promises.readFile(`${projectPath}/tree.json`, 'utf8');
-    const treeParsed = JSON.parse(fileTree);
-    const tree = new Tree(metadata.name, projectPath, metadata.scan_root);
-    tree.loadTree(treeParsed.tree.rootFolder);
-    const dep = await treeService.getDependencyStatus();
-    dep.forEach((d) => {
-      tree.getRootFolder().setStatus(d.path, d.status as NodeStatus);
-    });
-    const n = await tree.getTree();
-    treeParsed.tree.rootFolder = n;
-    await fs.promises.writeFile(`${projectPath}/tree.json`, JSON.stringify(treeParsed), 'utf-8');
   }
 
   public async reScanWFP(
@@ -169,12 +124,6 @@ class RescanService {
         filesToUpdate,
       );
       if (resultLicenses) await modelProvider.model.result.insertResultLicense(resultLicenses);
-      /*    const dirtyFiles = await modelProvider.model.file.getDirty();
-      if (dirtyFiles.length > 0) {
-        await modelProvider.model.inventory.deleteDirtyFileInventories(
-          dirtyFiles,
-        );
-      } */
 
       await modelProvider.model.result.deleteDirty();
       await modelProvider.model.file.deleteDirty();
---
+++ src/main/task/scanner/dependency/ReScanDependencyTask.ts
@@ -0,0 +1,42 @@
+import fs from 'fs';
+import { Tree } from '../../../workspace/tree/Tree';
+import { rescanService } from '../../../services/RescanService';
+import { treeService } from '../../../services/TreeService';
+import { NodeStatus } from '../../../workspace/tree/Node';
+import { modelProvider } from '../../../services/ModelProvider';
+import { DependencyTask } from './DependencyTask';
+
+export class ReScanDependencyTask extends DependencyTask {
+  // @Override
+  public async run(params: void): Promise<boolean> {
+    await super.run();
+
+    // Delete dirty dependencies inventories
+    await modelProvider.model.inventory.deleteDirtyDependencyInventories();
+
+    // Delete unused components
+    await rescanService.deleteUnusedComponents();
+
+    // Update the dependencies status on file tree
+    await this.updateDependenciesOnFileTree(this.project.getMyPath());
+
+    return true;
+  }
+
+  private async updateDependenciesOnFileTree(projectPath: string) {
+    // update dependency status on file tree
+    const m = await fs.promises.readFile(`${projectPath}/metadata.json`, 'utf8');
+    const metadata = JSON.parse(m);
+    const fileTree = await fs.promises.readFile(`${projectPath}/tree.json`, 'utf8');
+    const treeParsed = JSON.parse(fileTree);
+    const tree = new Tree(metadata.name, projectPath, metadata.scan_root);
+    tree.loadTree(treeParsed.tree.rootFolder);
+    const dep = await treeService.getDependencyStatus();
+    dep.forEach((d) => {
+      tree.getRootFolder().setStatus(d.path, d.status as NodeStatus);
+    });
+    const n = await tree.getTree();
+    treeParsed.tree.rootFolder = n;
+    await fs.promises.writeFile(`${projectPath}/tree.json`, JSON.stringify(treeParsed), 'utf-8');
+  }
+}
