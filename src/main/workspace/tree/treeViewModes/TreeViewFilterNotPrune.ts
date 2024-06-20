0d1808489568c45c75e765986568d3b8a22bc752
Agustin Isasmendi <isasmendi.agus@gmail.com>
2024-06-20 17:05:04 +0200

    feat: SP-921 Add dependency filter on file tree [backend]

--- src/main/workspace/tree/treeViewModes/TreeViewFilterNotPrune.ts
+++ src/main/workspace/tree/treeViewModes/TreeViewFilterNotPrune.ts
@@ -1,11 +1,20 @@
 import Node from '../Node';
 import { TreeViewFilter } from './TreeViewFilter';
+import { DependencyHighlighterVisitor } from '../visitor/DependencyHighlighterVisitor';
 
 export class TreeViewFilterNotPrune extends TreeViewFilter {
-  public async getTree(node: Node): Promise<Node>{
+  public async getTree(node: Node): Promise<Node> {
+    // TODO: Adjust all the filters to the visitor pattern.
+    if (this.filter?.usage === 'dependency') {
+      const root = node.getClone();
+      root.accept(new DependencyHighlighterVisitor());
+      return root;
+    }
+
     const files = await this.getFiles();
     const tree = node.getClone();
     tree.filter(files);
+
     return tree;
   }
 }
--- src/main/workspace/tree/treeViewModes/TreeViewFilterPrune.ts
+++ src/main/workspace/tree/treeViewModes/TreeViewFilterPrune.ts
@@ -1,10 +1,18 @@
-
 import Folder from '../Folder';
 import Node from '../Node';
 import { TreeViewFilter } from './TreeViewFilter';
+import { OnlyKeepDependencyVisitor } from '../visitor/OnlyKeepDependencyVisitor';
 
 export class TreeViewFilterPrune extends TreeViewFilter {
   public async getTree(node: Node): Promise<Node> {
+
+    // TODO: Adjust all the filters to the visitor pattern.
+    if (this.filter?.usage === 'dependency') {
+      const root = node.getClone();
+      root.accept(new OnlyKeepDependencyVisitor());
+      return root;
+    }
+
     const files = await this.getFiles();
     let tree = node.getClonePath(files);
     if (!tree) {
