33583cb284e73a9b3ac831fbb88ddd646e34c668
Agustin Isasmendi <isasmendi.agus@gmail.com>
2024-06-25 13:58:06 +0200

    feat: add two sections for dependency and components

--- src/renderer/features/workbench/components/DependencyManifestFileCard/DependencyManifestFileCard.tsx
+++ src/renderer/features/workbench/components/DependencyManifestFileCard/DependencyManifestFileCard.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import { DependencyManifestFile } from '@api/types';
+import { Button, Tooltip } from '@mui/material';
+import GavelIcon from '@mui/icons-material/Gavel';
+import path from 'path';
 
 interface DependencyManifestFileCardProps {
   dependencyManifestFile: DependencyManifestFile;
 }
 
 const DependencyManifestFileCard = ({ dependencyManifestFile }: DependencyManifestFileCardProps) => {
+  // const dependencyName = /[^/]+$/.exec(dependencyManifestFile.path);
+  const dependencyName = dependencyManifestFile.path;
   return (
-    <div>{dependencyManifestFile.path}</div>
+    <Tooltip title={dependencyManifestFile.path} enterDelay={650}>
+      <div className='dependency-card-content'>
+        <section className='content'>
+          <span>{dependencyName}</span>
+        </section>
+      </div>
+    </Tooltip>
   );
 };
 
