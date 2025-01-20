719cb1236a5f7ab47237d89b3362bef7f8119ffb
Jeronimo Ortiz <166400360+ortizjeronimo@users.noreply.github.com>
2025-01-20 09:38:02 -0300

    updated documentation theme and logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ author = 'Scan Open Source Solutions SL'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx_rtd_theme']
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -23,6 +23,6 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-html_logo = 'scanosslogo.png'
+html_logo = 'scanosslogo.jpg'
