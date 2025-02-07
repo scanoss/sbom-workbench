ad4efe0d1fd8bef89a2f40920f806a6f819390ed
Agustin Groh <agusgroh@gmail.com>
2025-02-07 11:54:20 -0300

    chore:ES-186 Implement chunked requests for component services

--- src/main/services/LicenseService.ts
+++ src/main/services/LicenseService.ts
@@ -1,12 +1,12 @@
 import { LicenseDTO, NewLicenseDTO } from '@api/dto';
+import { HttpClient } from 'scanoss';
+import log from 'electron-log';
 import { licenses } from '../../../assets/data/licenses';
 import { modelProvider } from './ModelProvider';
 import { licenseHelper } from '../helpers/LicenseHelper';
-import { HttpClient, HttpProxy } from 'scanoss';
 import { getHttpConfig } from './utils/httpUtil';
 import { userSettingService } from './UserSettingService';
-import { workspace } from '../../main/workspace/Workspace';
-import log from 'electron-log';
+import { workspace } from '../workspace/Workspace';
 import { LicenseObligation } from '../../api/types';
 
 class LicenseService {
@@ -34,29 +34,27 @@ class LicenseService {
   }
 
   public async getLicenseObligations(spdxid: string): Promise<LicenseObligation> {
-
     const project = workspace.getOpenedProjects()[0];
     const {
       DEFAULT_API_INDEX,
-      APIS
+      APIS,
     } = userSettingService.get();
-   
+
     const scanossHttp = new HttpClient(getHttpConfig());
 
-    const URL = project.getApi() ?  project.getApi() : APIS[DEFAULT_API_INDEX].URL;
- 
+    const URL = project.getApi() ? project.getApi() : APIS[DEFAULT_API_INDEX].URL;
+
     const obligationURL = `${URL}/license/obligations/${spdxid}`;
 
     const response = await scanossHttp.get(obligationURL);
 
     if (!response.ok) {
-        log.error("[ License obligations ]", response.statusText);
-        throw new Error(response.statusText);
+      log.error('[ License obligations ]', response.statusText);
+      throw new Error(response.statusText);
     }
-   
-    return await response.json();
+
+    return response.json();
   }
-  
 }
 
 export const licenseService = new LicenseService();
