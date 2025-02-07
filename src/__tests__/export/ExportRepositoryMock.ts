60b0b52ecafb243e2ac819ec80c51866ff6a3061
Agustin Groh <agusgroh@gmail.com>
2025-02-07 11:39:36 -0300

    chore:ES-182 Adds unit test

--- src/__tests__/export/ExportRepositoryMock.ts
+++ src/__tests__/export/ExportRepositoryMock.ts
@@ -1,16 +1,32 @@
 import { ExportRepository } from '../../main/modules/export/Repository/ExportRepository';
 import { ExportComponentData } from '../../main/model/interfaces/report/ExportComponentData';
 import { DecisionData } from '../../main/model/interfaces/report/DecisionData';
+import { ComponentVulnerability } from '../../main/model/entity/ComponentVulnerability';
+import { detectedVulnerabilityData, identifiedVulnerabilityData } from './mocks/vulnerability.model.mock';
 
 export class ExportRepositoryMock implements ExportRepository {
   private mockDecisionData: Array<DecisionData>;
 
+  private detectedVulnerabilityData: Array<ComponentVulnerability>;
+
+  private identifiedVulnerabilityData: Array<ComponentVulnerability>;
+
   constructor() {
     this.mockDecisionData = [];
+    this.detectedVulnerabilityData = detectedVulnerabilityData;
+    this.identifiedVulnerabilityData = identifiedVulnerabilityData;
   }
 
   public setDecisionMockData(data: Array<DecisionData>) {
     this.mockDecisionData = data;
+  }
+
+  public setIdentifiedMockData(data: Array<ComponentVulnerability>) {
+    this.identifiedVulnerabilityData = data;
+  }
+
+  public setDetectedVulnerabilityData(data: Array<ComponentVulnerability>) {
+    this.detectedVulnerabilityData = data;
   }
 
   public async getDecisionData(): Promise<Array<DecisionData>> {
@@ -32,5 +48,13 @@ export class ExportRepositoryMock implements ExportRepository {
 
   getWfpData(): Promise<string> {
     return Promise.resolve('');
+  }
+
+  getDetectedVulnerability(): Promise<Array<ComponentVulnerability>> {
+    return Promise.resolve(this.detectedVulnerabilityData);
+  }
+
+  getIdentifiedVulnerability(): Promise<Array<ComponentVulnerability>> {
+    return Promise.resolve(this.identifiedVulnerabilityData);
   }
 }
