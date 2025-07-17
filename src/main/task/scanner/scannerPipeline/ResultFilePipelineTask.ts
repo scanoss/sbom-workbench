332b67070a07e5d3beb89b16b3525a5bc64d4f0a
Agustin Groh <agusgroh@gmail.com>
2025-07-17 07:44:49 -0300

    chore:SP-2924 Scans vulnerabilities on raw result import

--- src/main/task/scanner/scannerPipeline/ResultFilePipelineTask.ts
+++ src/main/task/scanner/scannerPipeline/ResultFilePipelineTask.ts
@@ -5,12 +5,17 @@ import { ResultFileTreeTask } from '../../importFileResult/ResultFileTreeTask';
 import { ComponentImportTask } from '../../ComponentImportation/ComponentImportTask';
 import { ResultDependencyTask } from '../../importFileResult/ResultDependencyTask';
 import { SetupFileResultImportTask } from '../../importFileResult/SetupFileResultImportTask';
+import { VulnerabilitiesTask } from '../vulnerability/VulnerabilitiesTask';
+import { CryptographyTask } from '../cryptography/CryptographyTask';
+import { LocalCryptographyTask } from '../cryptography/LocalCryptographyTask';
+import { Scanner } from '../types';
+import ScannerType = Scanner.ScannerType;
 
 export class ResultFilePipelineTask extends ScannerPipeline {
 
   public async run(project: Project): Promise<boolean> {
     log.info("[Result File Pipeline Task init]...")
-
+    const { metadata } = project;
     // Setup
     this.queue.push(new SetupFileResultImportTask(project));
 
@@ -23,6 +28,8 @@ export class ResultFilePipelineTask extends ScannerPipeline {
     // Dependency
     this.queue.push(new ResultDependencyTask(project));
 
+    // Vulnerabilities
+    if (metadata.getScannerConfig().type.includes(ScannerType.VULNERABILITIES)) this.queue.push(new VulnerabilitiesTask(project));
 
     for await (const [index, task] of this.queue.entries()) {
       await this.executeTask(task, index);
