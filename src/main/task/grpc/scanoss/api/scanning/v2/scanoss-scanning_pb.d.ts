95f591219819c48971602e6c4cc05bb86fb624d9
Agustin Groh <agusgroh@gmail.com>
2025-09-05 14:45:11 -0300

    chore:Sp-3188 remove gRPC library and implementation

--- src/main/task/grpc/scanoss/api/scanning/v2/scanoss-scanning_pb.d.ts
+++
@@ -1,6 +0,0 @@
-// package: scanoss.api.scanning.v2
-// file: scanoss/api/scanning/v2/scanoss-scanning.proto
-
-import * as jspb from "google-protobuf";
-import * as scanoss_api_common_v2_scanoss_common_pb from "../../../../scanoss/api/common/v2/scanoss-common_pb";
-
